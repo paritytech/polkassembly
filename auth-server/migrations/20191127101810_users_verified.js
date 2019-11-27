2e31b46ce0b2f849caa014d0f6ba5d053e535c90
Nikhil Ranjan <niklabh811@gmail.com>
2019-11-27 13:59:01 +0100

    lint fix

--- auth-server/migrations/20191127101810_users_verified.js
+++ auth-server/migrations/20191127101810_users_verified.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex) {
-    return knex.schema.table('users', (table) => {
-        table.boolean('verified').defaultTo(false)
-    })
+	return knex.schema.table('users', (table) => {
+		table.boolean('verified').defaultTo(false)
+	})
 }
 
 exports.down = function(knex) {
-    return knex.schema.table('users', function (table) {
-        table.dropColumn('verified')
-    })
+	return knex.schema.table('users', function (table) {
+		table.dropColumn('verified')
+	})
 }
--- auth-server/src/services/email.ts
+++ auth-server/src/services/email.ts
@@ -9,41 +9,41 @@ const FROM = 'noreply@polkassembly.io'
 const DOMAIN = 'http://polkassembly.io'
 
 if (apiKey) {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+	sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 }
 
 export const sendVerificationEmail = (user: User, token: VerifyToken) => {
-    if (!apiKey) {
-        console.warn('Verification Email not sent due to missing API key')
-        return
-    }
+	if (!apiKey) {
+		console.warn('Verification Email not sent due to missing API key')
+		return
+	}
 
-    if (!EmailValidator.validate(user.email)) {
-        console.warn('Verification Email not sent due to invalid email')
-        return
-    }
+	if (!EmailValidator.validate(user.email)) {
+		console.warn('Verification Email not sent due to invalid email')
+		return
+	}
 
-    const verifyUrl = `${DOMAIN}/verify-account?token=${token.token}`
-    const text = `
-        <p>
-            Welcome aboard ${user.name}!<br/><br/>
+	const verifyUrl = `${DOMAIN}/verify-account?token=${token.token}`
+	const text = `
+		<p>
+			Welcome aboard ${user.name}!<br/><br/>
 
-            For security purposes, please confirm your email address here - <a target="_blank" href=${verifyUrl}>verify your account</a><br/><br/>
+			For security purposes, please confirm your email address here - <a target="_blank" href=${verifyUrl}>verify your account</a><br/><br/>
 
-            See you soon,<br/><br/>
-            Polkassembly Team
-        </p>
-    `
+			See you soon,<br/><br/>
+			Polkassembly Team
+		</p>
+	`
 
-    const msg = {
-        to: user.email,
-        from: FROM,
-        subject: 'Verify your email address',
-        text,
-        html: text,
-    }
+	const msg = {
+		to: user.email,
+		from: FROM,
+		subject: 'Verify your email address',
+		text,
+		html: text,
+	}
 
-    console.log(`Sending verification email to ${user.email}`)
+	console.log(`Sending verification email to ${user.email}`)
 
-    sgMail.send(msg);
+	sgMail.send(msg);
 }
