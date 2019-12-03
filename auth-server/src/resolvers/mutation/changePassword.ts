e21bebee69527febde8e8f9782e6096a5319785b
Nikhil Ranjan <niklabh811@gmail.com>
2019-12-03 14:16:31 +0530

    added password validation

--- auth-server/src/resolvers/mutation/changePassword.ts
+++ auth-server/src/resolvers/mutation/changePassword.ts
@@ -1,3 +1,5 @@
+import { UserInputError } from 'apollo-server'
+
 import getTokenFromReq from '../../utils/getTokenFromReq'
 import AuthService from '../../services/auth'
 import { Context } from '../../types'
@@ -5,6 +7,11 @@ import { Context } from '../../types'
 export default async (_, args, ctx: Context) => {
 	const token = getTokenFromReq(ctx.req)
 	const { oldPassword, newPassword } = args
+
+	if (newPassword.length < 6) {
+		throw new UserInputError('Your password must be at least 6 characters long.')
+	}
+
 	const authServiceInstance = new AuthService()
 	await authServiceInstance.ChangePassword(token, oldPassword, newPassword)
 
--- auth-server/src/resolvers/mutation/resetPassword.ts
+++ auth-server/src/resolvers/mutation/resetPassword.ts
@@ -1,8 +1,14 @@
 import AuthService from '../../services/auth'
 import { Context } from '../../types'
+import { UserInputError } from 'apollo-server'
 
 export default async (_, args, ctx: Context) => {
-	const { token, newPassword } = args
+    const { token, newPassword } = args
+
+    if (newPassword.length < 6) {
+		throw new UserInputError('Your password must be at least 6 characters long.')
+    }
+
 	const authServiceInstance = new AuthService()
 	await authServiceInstance.ResetPassword(token, newPassword)
 
