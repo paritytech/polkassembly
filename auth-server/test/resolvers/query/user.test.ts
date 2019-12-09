368819d4f354e16aec27237c31ce09a0fa99e1a7
Nikhil Ranjan <niklabh811@gmail.com>
2019-12-09 20:26:28 +0530

    added test suite

---
+++ auth-server/test/resolvers/query/user.test.ts
@@ -0,0 +1,10 @@
+import 'mocha'
+import { expect } from 'chai'
+
+
+describe('Hello function', () => {
+	it('should return hello world', () => {
+		const result = 'Hello World!'
+		expect(result).to.equal('Hello World!');
+	});
+});
