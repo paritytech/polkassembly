980124f1a2aa576e45ede78eca6d15a326339de3
thib <thibaut.sardan@gmail.com>
2019-12-12 14:12:47 +0100

    lint

--- auth-server/src/utils/validateEmail.ts
+++ auth-server/src/utils/validateEmail.ts
@@ -1,5 +1,5 @@
 export default (email: string): boolean => {
-	const regexp = /^[A-Z0-9_'%=+!`#~$*?^{}&|-]+([\.][A-Z0-9_'%=+!`#~$*?^{}&|-]+)*@[A-Z0-9-]+(\.[A-Z0-9-]+)+$/i
+	const regexp = /^[A-Z0-9_'%=+!`#~$*?^{}&|-]+([.][A-Z0-9_'%=+!`#~$*?^{}&|-]+)*@[A-Z0-9-]+(\.[A-Z0-9-]+)+$/i
 
 	return regexp.test(email)
 }
