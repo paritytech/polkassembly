8ddb7e0b39d20d3287964d82cfdda621a4a64067
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2019-12-12 18:48:37 +0100

    Update validateEmail.ts

--- auth-server/src/utils/validateEmail.ts
+++ auth-server/src/utils/validateEmail.ts
@@ -1,4 +1,3 @@
-// dumb edit to trigger CI
 export default (email: string): boolean => {
 	const regexp = /^[A-Z0-9_'%=+!`#~$*?^{}&|-]+([.][A-Z0-9_'%=+!`#~$*?^{}&|-]+)*@[A-Z0-9-]+(\.[A-Z0-9-]+)+$/i
 
