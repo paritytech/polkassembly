4891715cc4d3503a80f540ef2ef19fec333ff0a5
thib <thibaut.sardan@gmail.com>
2019-12-13 19:33:35 +0100

    add edited

--- front-end/src/components/PostContent.tsx
+++ front-end/src/components/PostContent.tsx
@@ -3,16 +3,19 @@ import ReactMarkdown from 'react-markdown';
 import * as moment from 'moment'
 
 import { PostFragment } from '../generated/graphql';
+import { Popup } from 'semantic-ui-react';
 
 const PostContent = ({ post } : {post: PostFragment}) => {
-	const { author, content, created_at, title } = post;
+	const { author, content, created_at, title, updated_at } = post;
 
 	if (!author || !author.username || !content) return <div>Post not available</div>
-
 	return (
 		<>
 			<h3>{title}</h3>
-			<div className='post_info'>posted by <strong>{author.username}</strong> {moment.default(created_at, 'YYYY-MM-DDTHH:mm:ss.SSS').fromNow()}</div>
+			<div className='post_info'>
+				posted by <strong>{author.username}</strong> {moment.default(created_at, 'YYYY-MM-DDTHH:mm:ss.SSS').fromNow()}
+				{updated_at === created_at ? null : <Popup content={moment.default(updated_at, 'YYYY-MM-DDTHH:mm:ss.SSS').fromNow()} trigger={<span> (edited)</span>}/>}
+			</div>
 			<ReactMarkdown className='post_content' source={content} />
 		</>
 	);
