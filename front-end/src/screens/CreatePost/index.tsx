77373b5c8aaa47e0364799abe4c7756136a74f95
thib <thibaut.sardan@gmail.com>
2019-12-14 16:10:36 +0100

    linting

--- front-end/src/screens/CreatePost/index.tsx
+++ front-end/src/screens/CreatePost/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import { Button, Container, Grid } from 'semantic-ui-react';
 import styled from 'styled-components';
 
