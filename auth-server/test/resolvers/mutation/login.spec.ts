179666ac7be4fbcf1cb7dab0229eaf5bfb355ea5
Nikhil Ranjan <niklabh811@gmail.com>
2019-12-17 17:08:46 +0530

    Added signup/login tests (#120)
    * added signup test
    
    * added email check test
    
    * added login tests
    
    * Added email validate tests (#121)
    
    * not testing text
    
    * stray comment
    
    * removed error message test
    
    * fix lint for regexp
    
    * change name test and other test fixes (#124)
    
    * change name test and other test fixes
    
    * added test for fake token error
    
    * removed unused import
    
    * unnecessary escape

---
+++ auth-server/test/resolvers/mutation/login.spec.ts
@@ -0,0 +1,63 @@
+import 'mocha'
+import { expect } from 'chai'
+import signup from '../../../src/resolvers/mutation/signup'
+import login from '../../../src/resolvers/mutation/login'
+import User from '../../../src/model/User'
+import { Context } from '../../../src/types'
+import { AuthenticationError } from 'apollo-server'
+
+describe('login mutation', () => {
+	let signupResult
+	let fakectx: Context = {
+		req: {},
+		res: {
+			cookie: () => {}
+		}
+	} as any
+	const email = 'test@email.com'
+	const password = 'testpass'
+	const username = 'testuser'
+	const name = 'test name'
+
+	before(async () => {
+		signupResult = await signup(null, { email, password, username, name }, fakectx)
+	})
+
+	after(async () => {
+		await User
+			.query()
+			.where({ id: signupResult.user.id })
+			.del()
+	})
+
+	it('should be able to login', async () => {
+		const result = await login(null, { username, password }, fakectx)
+
+		expect(result.user.id).to.exist
+		expect(result.user.id).to.a('number')
+		expect(result.user.email).to.equal(email)
+		expect(result.user.name).to.equal(name)
+		expect(result.user.username).to.equal(username)
+		expect(result.token).to.exist
+		expect(result.token).to.be.a('string')
+	})
+
+	it('should throw an error if username doesn\'t exist', async () => {
+		try {
+			await login(null, { username: 'notexist', password }, fakectx)
+		} catch (error) {
+			expect(error).to.exist
+			expect(error).to.be.an.instanceof(AuthenticationError)
+		}
+	})
+
+	it('should throw an error if password is wrong', async () => {
+		try {
+			await login(null, { username, password: 'wrong' }, fakectx)
+		} catch (error) {
+			expect(error).to.exist
+			expect(error).to.be.an.instanceof(AuthenticationError)
+		}
+	})
+
+})
---
+++ auth-server/test/resolvers/mutation/signup.spec.ts
@@ -0,0 +1,70 @@
+import 'mocha'
+import { expect } from 'chai'
+import signup from '../../../src/resolvers/mutation/signup'
+import User from '../../../src/model/User'
+import { Context } from '../../../src/types'
+import { ForbiddenError } from 'apollo-server'
+
+describe('signup mutation', () => {
+	const fakectx: Context = {
+		req: {},
+		res: {
+			cookie: () => {}
+		}
+	} as any
+	const email = 'test@email.com'
+	const password = 'testpass'
+	const username = 'testuser'
+	const name = 'test name'
+	const salt = 'testsalt'
+
+	it('should be able to signup', async () => {
+		const result = await signup(null, { email, password, username, name }, fakectx)
+		expect(result.user.id).to.exist
+		expect(result.user.id).to.a('number')
+		expect(result.user.email).to.equal(email)
+		expect(result.user.name).to.equal(name)
+		expect(result.user.username).to.equal(username)
+		expect(result.token).to.exist
+		expect(result.token).to.be.a('string')
+
+		await User
+			.query()
+			.where({ id: result.user.id })
+			.del()
+	})
+
+	it('should throw an error if user with username/email already exist', async () => {
+		const dbUser = await User
+			.query()
+			.allowInsert('[email, password, username, name]')
+			.insert({
+				email,
+				password,
+				salt,
+				username,
+				name
+			})
+			.returning('*')
+
+		try {
+			await signup(null, { email, password, username, name }, fakectx)
+		} catch (error) {
+			expect(error).to.exist
+			expect(error).to.be.an.instanceof(ForbiddenError)
+		}
+
+		try {
+			await signup(null, { email, password, username: 'newuser', name }, fakectx)
+		} catch (error) {
+			expect(error).to.exist
+			expect(error).to.be.an.instanceof(ForbiddenError)
+		}
+
+		await User
+			.query()
+			.where({ id: dbUser.id })
+			.del()
+	})
+
+})
