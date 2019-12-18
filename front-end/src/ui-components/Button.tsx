cdc06d2e96880b9911ec9c36b9d79debdce5d450
erler <mail@michelerler.com>
2019-12-18 17:38:13 +0100

    Update Button.tsx

--- front-end/src/ui-components/Button.tsx
+++ front-end/src/ui-components/Button.tsx
@@ -19,32 +19,25 @@ export default styled(Button)`
 		border-radius: 0.3rem;
 		border: none;
 		padding: 0.7rem 1rem;
-		color: #fff;
-		background-color: #EB5757;
+		&.ui.primary.button, &.active, &:active {
+			color: #fff;
+			background-color: #EB5757;
+			&:hover, &:focus {
+				background-color: #CC3D3D;
+				color: #FFF;
+				outline: none;
+			}
+		}
+		&.ui.secondary.button, &.active, &:active {
+			color: #EB5757;
+			background-color: #FFF;
+			border: 2px solid #EB5757;
+			&:hover, &:focus {
+				background-color: #CC3D3D;
+				color: #FFF;
+				border: 2px solid #CC3D3D;
+				outline: none;
+			}
+		}
 	}
-
-	&:hover, &:focus, &.ui.primary.button:hover, &.ui.primary.button:focus {
-        background-color: #CC3D3D;
-        color: #FFF;
-        outline: none;
-    }
-
-    &.ui.secondary.button, &.active, &:active {   
-        font-family: 'Roboto Mono';
-        font-size: 1.45rem;
-        font-weight: 500;
-        text-transform: uppercase;
-        border-radius: 0.3rem;
-        border: 2px solid #EB5757;
-        padding: 0.7rem 1rem;
-		color: #EB5757;
-		background-color: #FFF;
-    }
-
-    &:hover, &:focus, &.ui.secondary.button:hover, &.ui.secondary.button:focus {
-        background-color: #CC3D3D;
-        color: #FFF;
-        border: 2px solid #CC3D3D;
-        outline: none;
-    }
 `;
