a5b63847ce9293884b4db9fc7c8f5f5f4277e286
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2019-12-20 16:09:07 +0100

    lint

--- front-end/src/screens/SignupForm/index.tsx
+++ front-end/src/screens/SignupForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import { Grid } from 'semantic-ui-react';
 import styled from 'styled-components';
 
