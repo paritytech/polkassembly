e12496ef1377a9fb31cd12f11cc53ee63f0a7692
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2019-12-20 17:47:13 +0100

    Better loader (#147)
    * init
    
    * Update front-end/src/ui-components/Loader.tsx

--- front-end/src/screens/Post/index.tsx
+++ front-end/src/screens/Post/index.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+
 import { usePostAndCommentsQuery } from '../../generated/graphql';
 import Post from './Post';
+import Loader from '../../ui-components/Loader';
 
 export default () => {
 	const { id } = useParams();
-	// cast id as a number or 0
 	const idNumber = Number(id) || 0;
-	const { data, error, loading, refetch } = usePostAndCommentsQuery({ variables: { 'id': idNumber } });
-
-	if (loading) {
-		return <div>Loading...</div>;
-	}
+	const { data, error, refetch } = usePostAndCommentsQuery({ variables: { 'id': idNumber } });
 
 	if (error) return <div> Error: {error.message}</div>;
 
 	if (data) return <Post data={data} refetch={refetch} />;
 
-	return <div>Loading...</div>
+	return <Loader/>
 };
---
+++ front-end/src/ui-components/Loader.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Dimmer, Loader as SUILoader } from 'semantic-ui-react'
+import styled from 'styled-components'
+
+interface Props{
+    className?: string
+}
+const Loader = ({ className }: Props) => {
+	return (
+		<Dimmer inverted active className={className}>
+			<SUILoader inverted>Loading</SUILoader>
+		</Dimmer>
+	)
+}
+
+export default styled(Loader)`
+    background-color: inherit!important;
+`
