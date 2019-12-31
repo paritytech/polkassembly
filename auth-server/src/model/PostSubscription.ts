c7b7be43c5963a2df405d926a50fac246ee04030
Nikhil Ranjan <niklabh811@gmail.com>
2019-12-31 17:00:23 +0530

    Added post subscribe unsubscribe mutation

---
+++ auth-server/src/model/PostSubscription.ts
@@ -0,0 +1,39 @@
+import { Model } from 'objection'
+import connection from './connection'
+
+Model.knex(connection)
+
+export default class PasswordResetToken extends Model {
+	readonly id!: number
+	user_id!: number
+	post_id!: number
+
+	static get tableName () {
+		return 'post_subscription'
+	}
+
+	static get idColumn () {
+		return 'id'
+	}
+
+	getToken () {
+		return {
+			id: this.id,
+			user_id: this.user_id,
+			post_id: this.post_id
+		}
+	}
+
+	static get jsonSchema () {
+		return {
+			type: 'object',
+			required: ['user_id', 'post_id'],
+			properties: {
+				id: { type: 'integer' },
+				user_id: { type: 'integer' },
+				post_id: { type: 'integer' }
+			}
+		}
+	}
+}
+
