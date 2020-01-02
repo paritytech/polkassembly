8e1c98c3936d4d6db817081c37255eabfd173995
thib <thibaut.sardan@gmail.com>
2020-01-02 14:19:31 +0100

    update and lint master

--- auth-server/test/services/email/email.spec.ts
+++ auth-server/test/services/email/email.spec.ts
@@ -1,19 +1,19 @@
-import 'mocha'
-import { expect } from 'chai'
-import rewiremock from 'rewiremock'
+import 'mocha';
+import { expect } from 'chai';
+import rewiremock from 'rewiremock';
 
-import EmailVerificationToken from '../../../src/model/EmailVerificationToken'
-import PasswordResetToken from '../../../src/model/PasswordResetToken'
-import User from '../../../src/model/User'
+import EmailVerificationToken from '../../../src/model/EmailVerificationToken';
+import PasswordResetToken from '../../../src/model/PasswordResetToken';
+import User from '../../../src/model/User';
 
-const noop = () => {}
+const noop = () => {};
 
 describe('Email Service', () => {
-	const email = 'test@email.com'
-	const password = 'testpass'
-	const username = 'testuser'
-	const name = 'test name'
-	const salt = 'testsalt'
+	const email = 'test@email.com';
+	const password = 'testpass';
+	const username = 'testuser';
+	const name = 'test name';
+	const salt = 'testsalt';
 
 	it('should send verification email', async () => {
 		const user = await User
@@ -26,7 +26,7 @@ describe('Email Service', () => {
 				username,
 				name
 			})
-			.returning('*')
+			.returning('*');
 		const token = await EmailVerificationToken
 			.query()
 			.allowInsert('[token, user_id, valid]')
@@ -34,38 +34,38 @@ describe('Email Service', () => {
 				token: 'test-token',
 				user_id: user.id,
 				valid: true
-			})
+			});
 
-		let message: any
+		let message: any;
 
 		rewiremock('@sendgrid/mail').with({
 			setApiKey: noop,
 			send: (msg) => {
-				message = msg
-				return Promise.resolve()
+				message = msg;
+				return Promise.resolve();
 			}
-		})
-		rewiremock.enable()
-		const { sendVerificationEmail } = require('../../../src/services/email')
-		rewiremock.disable()
+		});
+		rewiremock.enable();
+		const { sendVerificationEmail } = require('../../../src/services/email');
+		rewiremock.disable();
 
-		sendVerificationEmail(user, token)
+		sendVerificationEmail(user, token);
 
-		expect(message.to).to.equals('test@email.com')
-		expect(message.from).to.equals('noreply@polkassembly.io')
-		expect(message.subject).to.equals('Verify your email address')
-		expect(message.html).to.contains(`verify-email/${token.token}`)
+		expect(message.to).to.equals('test@email.com');
+		expect(message.from).to.equals('noreply@polkassembly.io');
+		expect(message.subject).to.equals('Verify your email address');
+		expect(message.html).to.contains(`verify-email/${token.token}`);
 
 		await User
 			.query()
 			.where({ id: user.id })
-			.del()
+			.del();
 
 		await EmailVerificationToken
 			.query()
 			.where({ id: token.id })
-			.del()
-	})
+			.del();
+	});
 
 	it('should send password reset email', async () => {
 		const user = await User
@@ -78,7 +78,7 @@ describe('Email Service', () => {
 				username,
 				name
 			})
-			.returning('*')
+			.returning('*');
 		const token = await PasswordResetToken
 			.query()
 			.allowInsert('[token, user_id, valid, expires]')
@@ -87,36 +87,36 @@ describe('Email Service', () => {
 				user_id: user.id,
 				valid: true,
 				expires: new Date().toISOString()
-			})
+			});
 
-		let message: any
+		let message: any;
 
 		rewiremock('@sendgrid/mail').with({
 			setApiKey: noop,
 			send: (msg) => {
-				message = msg
-				return Promise.resolve()
+				message = msg;
+				return Promise.resolve();
 			}
-		})
-		rewiremock.enable()
-		const { sendResetPasswordEmail } = require('../../../src/services/email')
-		rewiremock.disable()
+		});
+		rewiremock.enable();
+		const { sendResetPasswordEmail } = require('../../../src/services/email');
+		rewiremock.disable();
 
-		sendResetPasswordEmail(user, token)
+		sendResetPasswordEmail(user, token);
 
-		expect(message.to).to.equals('test@email.com')
-		expect(message.from).to.equals('noreply@polkassembly.io')
-		expect(message.subject).to.equals('Reset Your Password')
-		expect(message.html).to.contains(`reset-password?token=${token.token}`)
+		expect(message.to).to.equals('test@email.com');
+		expect(message.from).to.equals('noreply@polkassembly.io');
+		expect(message.subject).to.equals('Reset Your Password');
+		expect(message.html).to.contains(`reset-password?token=${token.token}`);
 
 		await User
 			.query()
 			.where({ id: user.id })
-			.del()
+			.del();
 
 		await PasswordResetToken
 			.query()
 			.where({ id: token.id })
-			.del()
-	})
-})
+			.del();
+	});
+});
