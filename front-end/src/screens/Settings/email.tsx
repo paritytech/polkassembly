1e291e2bc63f38b39b2cd04a1c87460bafe097c2
thib <thibaut.sardan@gmail.com>
2020-01-02 16:52:20 +0100

    init

--- front-end/src/screens/Settings/email.tsx
+++ front-end/src/screens/Settings/email.tsx
@@ -67,38 +67,40 @@ const Email = ({ className }: Props): JSX.Element => {
 	};
 
 	return (
-		<Form.Group className={className}>
-			<Form.Field width={10}>
-				<label>Email</label>
-				<input
-					value={email || ''}
-					onChange={onEmailChange}
-					placeholder='mail@example.com'
-					type='email'
-				/>
-				{error &&
+		<Form standalone={false}>
+			<Form.Group className={className}>
+				<Form.Field width={10}>
+					<label>Email</label>
+					<input
+						value={email || ''}
+						onChange={onEmailChange}
+						placeholder='mail@example.com'
+						type='email'
+					/>
+					{error &&
 				<>
 					<br/><div>{error.message}</div>
 				</>
-				}
-				{!currentUser.email_verified &&
+					}
+					{!currentUser.email_verified &&
 				<div className={'warning-text'}>
 					<div><Icon name='warning circle' />Your email is not verified.</div>
 				</div>
-				}
-			</Form.Field>
-			<Form.Field width={2}>
-				<label>&nbsp;</label>
-				<Button
-					primary
-					disabled={loading}
-					onClick={handleClick}
-					type='submit'
-				>
+					}
+				</Form.Field>
+				<Form.Field width={2}>
+					<label>&nbsp;</label>
+					<Button
+						primary
+						disabled={loading}
+						onClick={handleClick}
+						type='submit'
+					>
 					Change
-				</Button>
-			</Form.Field>
-		</Form.Group>
+					</Button>
+				</Form.Field>
+			</Form.Group>
+		</Form>
 	);
 };
 
