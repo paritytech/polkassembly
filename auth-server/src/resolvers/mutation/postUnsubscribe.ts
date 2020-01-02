8f71de18f3ee0939368332dd71f04a6740f0a2d5
Nikhil Ranjan <niklabh811@gmail.com>
2020-01-02 19:10:36 +0530

    added semicolons

--- auth-server/src/resolvers/mutation/postUnsubscribe.ts
+++ auth-server/src/resolvers/mutation/postUnsubscribe.ts
@@ -1,17 +1,17 @@
-import PostSubscription from '../../model/PostSubscription'
-import AuthService from '../../services/auth'
-import { Context  } from '../../types'
-import getTokenFromReq from '../../utils/getTokenFromReq'
-import messages from '../../utils/messages'
+import PostSubscription from '../../model/PostSubscription';
+import AuthService from '../../services/auth';
+import { Context  } from '../../types';
+import getTokenFromReq from '../../utils/getTokenFromReq';
+import messages from '../../utils/messages';
 
 interface argsType {
 	post_id: number
 }
 
 export default async (parent, { post_id }: argsType, ctx: Context)  => {
-	const token = getTokenFromReq(ctx.req)
-	const authServiceInstance = new AuthService()
-	const user = await authServiceInstance.GetUser(token)
+	const token = getTokenFromReq(ctx.req);
+	const authServiceInstance = new AuthService();
+	const user = await authServiceInstance.GetUser(token);
 
 	await PostSubscription
 		.query()
@@ -19,7 +19,7 @@ export default async (parent, { post_id }: argsType, ctx: Context)  => {
 			user_id: user.id,
 			post_id
 		})
-		.del()
+		.del();
 
-	return { message: messages.SUBSCRIPTION_REMOVE_SUCCESSFUL }
-}
+	return { message: messages.SUBSCRIPTION_REMOVE_SUCCESSFUL };
+};
