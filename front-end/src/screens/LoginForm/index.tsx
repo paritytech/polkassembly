d74db62ab087e56d5c65c146d86b08073231547e
thib <thibaut.sardan@gmail.com>
2020-01-06 14:55:09 +0100

    using uncontrolled input

--- front-end/src/screens/LoginForm/index.tsx
+++ front-end/src/screens/LoginForm/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { Controller, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { Grid } from 'semantic-ui-react';
 import styled from 'styled-components';
 
@@ -18,19 +18,13 @@ interface Props {
 }
 
 const LoginForm = ({ className }:Props): JSX.Element => {
-	const [username, setUsername] = useState<string | undefined>('');
-	const [password, setPassword] = useState<string | undefined>('');
 	const currentUser = useContext(UserDetailsContext);
 	const { history } = useRouter();
 	const [loginMutation, { loading, error }] = useLoginMutation({ context: { uri : process.env.REACT_APP_AUTH_SERVER_GRAPHQL_URL } });
-	const { control, errors, handleSubmit, register } = useForm();
+	const { errors, handleSubmit, register } = useForm();
 
-	const onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>[]) => setUsername(event[0].currentTarget.value);
-	const onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>[]) => setPassword(event[0].currentTarget.value);
-
-	const handleClick = ():void => {
-		// event.preventDefault();
-		// event.stopPropagation();
+	const handleSubmitForm = (data:Record<string, any>):void => {
+		const { username, password } = data;
 
 		if (username && password){
 			loginMutation({
@@ -53,41 +47,33 @@ const LoginForm = ({ className }:Props): JSX.Element => {
 		<Grid className={className}>
 			<Grid.Column only='tablet computer' tablet={2} computer={4} largeScreen={5} widescreen={6}/>
 			<Grid.Column mobile={16} tablet={12} computer={8} largeScreen={6} widescreen={4}>
-				<Form>
+				<Form onSubmit={handleSubmit(handleSubmitForm)}>
 					<h3>Login</h3>
 					<Form.Group>
 						<Form.Field width={16}>
 							<label>Username</label>
-							<Controller
-								as={<input
-									className={errors.username ? 'error' : ''}
-									placeholder='John'
-									type='text'
-								/>}
-								control={control}
+							<input
+								className={errors.username ? 'error' : ''}
 								name='username'
-								onChange={onUsernameChange}
-								rules={{ minLength: 3, required: true }}
+								placeholder='John'
+								ref={register({ minLength: 3, required: true })}
+								type='text'
 							/>
-							{errors.username && errors.username.type === 'required' && <span className={'errorText'}>{messages.VALIDATION_USERNAME_ERROR}</span>}
+							{errors.username && <span className={'errorText'}>{messages.VALIDATION_USERNAME_ERROR}</span>}
 						</Form.Field>
 					</Form.Group>
 
 					<Form.Group>
 						<Form.Field width={16}>
 							<label>Password</label>
-							<Controller
-								as={<input
-									className={errors.password ? 'error' : ''}
-									placeholder='Password'
-									type='password'
-								/>}
-								control={control}
+							<input
+								className={errors.password ? 'error' : ''}
 								name='password'
-								onChange={onPasswordChange}
-								rules={{ min: 6 }}
+								placeholder='Password'
+								ref={register({ minLength: 6 ,required: true })}
+								type='password'
 							/>
-							{errors.username && <span className={'errorText'}>{messages.VALIDATION_PASSWORD_ERROR}</span>}
+							{errors.password && <span className={'errorText'}>{messages.VALIDATION_PASSWORD_ERROR}</span>}
 
 							<div className='text-muted'>
 								<Link to='/request-reset-password'>Forgot your password?</Link>
@@ -98,7 +84,6 @@ const LoginForm = ({ className }:Props): JSX.Element => {
 						<Button
 							primary
 							disabled={loading}
-							onClick={handleSubmit(handleClick)}
 							type='submit'
 						>
 							Login
