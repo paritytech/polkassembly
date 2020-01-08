4fcc06393bff4d0a6593be608b5276d7996a3616
Nikhil Ranjan <niklabh811@gmail.com>
2020-01-08 18:29:13 +0530

    alphabetical order fixed; added READMe

--- auth-server/src/resolvers/index.ts
+++ auth-server/src/resolvers/index.ts
@@ -1,19 +1,19 @@
-import user from './query/user';
-import users from './query/users';
-import token from './query/token';
-import subscription from './query/subscription';
-import login from './mutation/login';
-import logout from './mutation/logout';
-import signup from './mutation/signup';
 import changeUsername from './mutation/changeUsername';
 import changeEmail from './mutation/changeEmail';
 import changePassword from './mutation/changePassword';
 import changeName from './mutation/changeName';
+import login from './mutation/login';
+import logout from './mutation/logout';
 import postSubscribe from './mutation/postSubscribe';
 import postUnsubscribe from './mutation/postUnsubscribe';
 import requestResetPassword from './mutation/requestResetPassword';
 import resetPassword from './mutation/resetPassword';
+import signup from './mutation/signup';
 import verifyEmail from './mutation/verifyEmail';
+import subscription from './query/subscription';
+import token from './query/token';
+import user from './query/user';
+import users from './query/users';
 
 export default {
 	Query: {
--- auth-server/src/schema/index.ts
+++ auth-server/src/schema/index.ts
@@ -1,55 +1,57 @@
 import { gql } from 'apollo-server-express';
 
-import userType from './type/user';
-import tokenType from './type/token';
-import messageType from './type/message';
-import changeResponseType from './type/changeResponse';
-import loginResponseType from './type/loginResponse';
-import subscriptionType from './type/subscription';
 import userQuery from './query/user';
 import usersQuery from './query/users';
 import subscription from './query/subscription';
 import tokenQuery from './query/token';
-import loginMutation from './mutation/login';
-import logoutMutation from './mutation/logout';
-import signupMutation from './mutation/signup';
+
+import changeResponseType from './type/changeResponse';
+import loginResponseType from './type/loginResponse';
+import messageType from './type/message';
+import subscriptionType from './type/subscription';
+import tokenType from './type/token';
+import userType from './type/user';
+
 import changeUsernameMutation from './mutation/changeUsername';
 import changeEmailMutation from './mutation/changeEmail';
 import changePasswordMutation from './mutation/changePassword';
 import changeNameMutation from './mutation/changeName';
+import loginMutation from './mutation/login';
+import logoutMutation from './mutation/logout';
 import postSubscribe from './mutation/postSubscribe';
 import postUnsubscribe from './mutation/postUnsubscribe';
 import requestResetPassword from './mutation/requestResetPassword';
 import resetPassword from './mutation/resetPassword';
+import signupMutation from './mutation/signup';
 import verifyEmail from './mutation/verifyEmail';
 
 export default gql`
-	${userType}
-	${tokenType}
 	${changeResponseType}
 	${loginResponseType}
 	${messageType}
 	${subscriptionType}
+	${tokenType}
+	${userType}
 
 	type Query {
-		${userQuery}
-		${usersQuery}
 		${subscription}
 		${tokenQuery}
+		${userQuery}
+		${usersQuery}
 	}
 
 	type Mutation {
-		${loginMutation}
-		${logoutMutation}
-		${signupMutation}
 		${changeUsernameMutation}
 		${changeEmailMutation}
 		${changePasswordMutation}
 		${changeNameMutation}
+		${loginMutation}
+		${logoutMutation}
 		${postSubscribe}
 		${postUnsubscribe}
 		${requestResetPassword}
 		${resetPassword}
+		${signupMutation}
 		${verifyEmail}
 	}
 `;
