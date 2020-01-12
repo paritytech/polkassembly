109ddd9d8be04edda58b0f17bbe64d65ccefea99
Nikhil Ranjan <niklabh811@gmail.com>
2020-01-12 20:39:53 +0530

    added migration for undo token

---
+++ auth-server/migrations/20200111182158_email_undo_token.js
@@ -0,0 +1,16 @@
+// Table to store tokens for email verification
+exports.up = function(knex) {
+	return knex.schema.createTable('email_undo_token', (table) => {
+		table.increments('id').primary().notNullable();
+		table.string('token').unique().notNullable();
+		table.strin('email').unique().notNullable();
+		table.integer('user_id').notNullable();
+		table.boolean('valid').notNullable();
+		table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
+		table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE').onUpdate('RESTRICT');
+	});
+};
+
+exports.down = function(knex) {
+	return knex.schema.dropTable('email_undo_token');
+};
