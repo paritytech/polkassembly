6a0a134fb4ec22198759b4e59ad8e23601169598
Nikhil Ranjan <niklabh811@gmail.com>
2020-01-14 03:37:54 +0530

    uuid columns

--- auth-server/migrations/20191127103957_email_verification_token.js
+++ auth-server/migrations/20191127103957_email_verification_token.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
 	return knex.schema.createTable('email_verification_token', (table) => {
 		table.increments('id').primary().notNullable();
-		table.string('token').unique().notNullable();
+		table.uuid('token').unique().notNullable();
 		table.integer('user_id').notNullable();
 		table.boolean('valid').notNullable();
 		table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
