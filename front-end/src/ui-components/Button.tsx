2bc81f992fc5f8146bc57ea4a324af1d153277c7
Michel Erler <7072141+erler@users.noreply.github.com>
2020-01-13 11:54:11 +0100

    social button (#220)

--- front-end/src/ui-components/Button.tsx
+++ front-end/src/ui-components/Button.tsx
@@ -16,60 +16,71 @@ export default styled(Button)`
 		border: none;
 		padding: 0.7rem 1rem;
 		&.ui.primary.button {
-			background-color: primary_red;
-			color: #FFF;
+			background-color: red_primary;
+			color: white;
 			&:hover, &:focus {
-				background-color: #D94C3D;
-				color: #FFF;
+				background-color: red_secondary;
+				color: white;
 				outline: none;
 			}
 		}
 		&.ui.primary.negative.button {
-			background-color: #FFF;
-			color: #FF5A47;
-			border: 0.1rem solid #FF5A47;
+			background-color: white;
+			color: red_primary;
+			border: 0.1rem solid red_primary;
 			&:hover, &:focus {
-				background-color: #FFF1F0;
-				color: #D94C3D;
+				background-color: red_light;
+				color: red_secondary;
 				border: 0.1rem solid #D94C3D;
 				outline: none;
 			}
 		}
 		&.ui.secondary.button {
-			background-color: #FFF;
-			color: #FF5A47;
+			background-color: white;
+			color: red_primary;
 			border: 0.1rem solid #FF5A47;
 			&:hover, &:focus {
-				background-color: #FFF1F0;
-				color: #D94C3D;
+				background-color: red_light;
+				color: red_secondary;
 				border: 0.1rem solid #D94C3D;
 				outline: none;
 			}
 		}
 		&.ui.secondary.negative.button {
-			background-color: #FFF;
+			background-color: white;
 			color: #E65140;
 			border: 0.1rem solid #E65140;
 			&:hover, &:focus {
 				background-color: #E66557;
-				color: #FFF;
-				border: 0.15rem solid #E66557;
+				color: white;
+				border: 0.1rem solid #E66557;
 				outline: none;
 			}	
 		}
 		&.ui.social.button {
-			background-color: #FFF;
-			color: #706D6D;
-			font-size: 1.2rem;
+			background-color: white;
+			color: grey_primary;
+			font-size: sm;
 			border: none;
 			padding: 0.7rem 0.7rem;
 			&:hover{
-				background-color: #EEE;
+				background-color: grey_light;
 				border: none;
 			}
 		}
 		&.ui.button>.icon:not(.button) {
 			opacity: 1;
+		}
+		&.ui.social.negative.button {
+				background-color: white;
+				color: red_primary;
+				font-size: sm;
+				border: none;
+				padding: 0.7rem 0.7rem;
+			&:hover {
+				background-color: red_light;
+				border: none;
+			}
 		}
 	}
 `;
