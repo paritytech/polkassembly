34836395a01fe36f68c0244617cfa3b3dc7a3695
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2020-01-16 09:21:26 +0100

    Create chain db watcher (#223)
    * init
    
    * rename
    
    * nits
    
    * nits
    
    * working subscription
    
    * adapt ports for nomidot
    
    * hasura migration
    
    * remote schema
    
    * add post mutation
    
    * cleanup
    
    * switch to onchainId
    
    * wrapping up
    
    * add rights to proposal_bot
    
    * add id to author
    
    * nit
    
    * no need for a token to check proposals
    
    * no env and readme
    
    * remove unneeded types
    
    * remove @polkadot deps
    
    * stray comments
    
    * sql in readme
    
    * Update chain-db-watcher/.gitignore
    
    * fix env variables
    
    * rename env vars
    
    * rename yarn-error.log
    
    * address other comments
    
    * no extra parens complains

---
+++ chain-db-watcher/src/queries.ts
@@ -0,0 +1,102 @@
+import gql from 'graphql-tag';
+
+export const proposalsSubscription = gql`
+    subscription proposalsSubscription{
+        proposal {
+            mutation
+                node {
+                    depositAmount
+                    method
+                    proposalId
+                    proposer
+                    proposalArguments {
+                        name
+                        value
+                    }
+                    section
+                }
+        }
+    }
+`;
+
+// e.g returns
+// {
+// 	"data": {
+// 	  "proposal": {
+// 		"mutation": "CREATED",
+// 		"node": {
+// 		  "method": "remark",
+// 		  "metaDescription": "[ Make some on-chain remark.]",
+// 		  "proposalId": 0,
+// 		  "section": "system",
+// 		  "depositAmount": "123000000000000",
+// 		  "proposalArguments": [
+// 			{
+// 			  "name": "_remark",
+// 			  "value": "0x00"
+// 			}
+// 		  ],
+// 		  "proposer": "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
+// 		}
+// 	  }
+// 	}
+//   }
+
+export const addPostAndProposalMutation = `
+    mutation addPostAndProposalMutation (
+        $onchain_proposal_id:Int!,
+        $author_id: Int!,
+        $proposer_address: String!,
+        $content: String!,
+        $title: String!,
+        $topic_id: Int!,
+        $type_id: Int!
+        ){
+        __typename
+        insert_onchain_proposals(objects: {
+            onchain_proposal_id: $onchain_proposal_id,
+            proposer_address: $proposer_address,
+            post: {data: {
+                author_id: $author_id,
+                content: $content,
+                title: $title,
+                topic_id: $topic_id,
+                type_id: $type_id
+            }
+        }}) {
+        returning {
+            id
+        }
+        }
+    }
+`;
+
+// returns
+// {
+// 	"data": {
+// 	  "insert_onchain_proposals": {
+// 		"returning": [
+// 		  {
+// 			"id": 1
+// 		  }
+// 		]
+// 	  },
+// 	  "__typename": "mutation_root"
+// 	}
+//   }
+
+export const getProposalQuery = `
+    query getProposals($onchain_proposal_id: Int!) {
+        onchain_proposals(where: {onchain_proposal_id: {_eq: $onchain_proposal_id}}) {
+            id
+        }
+    }
+`;
+
+export const loginMutation = `
+    mutation LOGIN($password: String!, $username: String!) {
+        login(password: $password, username: $username) {
+            token
+        }
+    }
+`;
