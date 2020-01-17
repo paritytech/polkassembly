6775f799339cc235dd26d29a2d86e3d5777dd87c
Michel Erler <7072141+erler@users.noreply.github.com>
2020-01-17 18:31:39 +0100

    Update front-end/src/screens/MenuBar/index.tsx
    Co-Authored-By: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>

--- front-end/src/screens/MenuBar/index.tsx
+++ front-end/src/screens/MenuBar/index.tsx
@@ -71,7 +71,7 @@ const MenuBar = ({ className } : Props): JSX.Element => {
 						Polkassembly
 					</Menu.Item>
 					<Menu.Item onClick={handleToggle} id='rightmenu'>
-						{!menuVisible ? <Icon name="sidebar" /> : <Icon name="times" />}
+						{!menuVisible ? <Icon name="sidebar" /> : <Icon name="close" />}
 					</Menu.Item>
 				</Menu>
 				<Sidebar.Pushable className={className} style={{ height:pushableHeight }}>
