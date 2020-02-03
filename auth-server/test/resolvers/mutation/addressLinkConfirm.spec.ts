ee909a64af6d30fc12dae1114c581ea1b45e8ae8
Nikhil Ranjan <niklabh811@gmail.com>
2020-02-03 17:07:56 +0530

    Address linking/unlinking test (#242)
    * Address linking test
    
    * added addresses test
    
    * added unlink test
    
    * Added address link confirm mutation test
    
    * moved signMessage to const
    
    * alice signature
    
    * fix
    
    * using address instead of uuid
    
    * using publicKey instead of uuid
    
    * using Alice
    
    * using Alice bis
    
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>

---
+++ auth-server/test/resolvers/mutation/addressLinkConfirm.spec.ts
@@ -0,0 +1,103 @@
+import { expect } from 'chai';
+import { AuthenticationError, ForbiddenError } from 'apollo-server';
+import 'mocha';
+
+import User from '../../../src/model/User';
+import Address from '../../../src/model/Address';
+import addressLinkStart from '../../../src/resolvers/mutation/addressLinkStart';
+import addressLinkConfirm from '../../../src/resolvers/mutation/addressLinkConfirm';
+import signup from '../../../src/resolvers/mutation/signup';
+import { Context } from '../../../src/types';
+import messages from '../../../src/utils/messages';
+
+describe('addressLinkConfirm mutation', () => {
+	let signupResult;
+	const fakectx: Context = {
+		req: {
+			headers: {}
+		},
+		res: {
+			cookie: () => {}
+		}
+	} as any;
+	const email = 'test@email.com';
+	const password = 'testpass';
+	const username = 'testuser';
+	const name = 'test name';
+
+	before(async () => {
+		signupResult = await signup(null, { email, password, username, name }, fakectx);
+		fakectx.req.headers.authorization = `Bearer ${signupResult.token}` // eslint-disable-line
+	});
+
+	after(async () => {
+		await User
+			.query()
+			.where({ id: signupResult.user.id })
+			.del();
+	});
+
+	it('should be able to confirm address link', async () => {
+		const network = 'kasuma';
+		const address = 'HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F'; //Alice
+		const signMessage = 'da194645-4daf-43b6-b023-6c6ce99ee709';
+		const signature = '0x048ffa02dd58557ab7f7ffb316ac75fa942d2bdb83f4480a6698a1f39d6fa1184dd85d95480bfab59f516de578b102a2b01b81ca0e69134f90e0cd08ada7ca88';
+
+		const linkStartRes = await addressLinkStart(null, { network, address }, fakectx);
+
+		await Address
+			.query()
+			.patch({
+				sign_message: signMessage
+			})
+			.findById(linkStartRes.address_id);
+
+		const linkConfirmRes = await addressLinkConfirm(null, { signature, address_id: linkStartRes.address_id }, fakectx);
+
+		const dbAddress = await Address
+			.query()
+			.where({ user_id: signupResult.user.id })
+			.first();
+
+		expect(dbAddress.public_key).to.exist;
+		expect(dbAddress.verified).to.be.true;
+
+		expect(linkConfirmRes.message).to.equal(messages.ADDRESS_LINKING_SUCCESSFUL);
+	});
+
+	it('should not be able to confirm address link with fake signature', async () => {
+		const network = 'kasuma';
+		const address = 'HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F'; // Alice
+		const signMessage = 'da194645-4daf-43b6-b023-6c6ce99ee709';
+		const signature = 'fake';
+
+		const linkStartRes = await addressLinkStart(null, { network, address }, fakectx);
+
+		await Address
+			.query()
+			.patch({
+				sign_message: signMessage
+			})
+			.findById(linkStartRes.address_id);
+
+		try {
+			await addressLinkConfirm(null, { signature, address_id: linkStartRes.address_id }, fakectx);
+		} catch (error) {
+			expect(error).to.exist;
+			expect(error).to.be.an.instanceof(ForbiddenError);
+			expect(error.message).to.eq(messages.ADDRESS_LINKING_FAILED);
+		}
+	});
+
+	it('should not be able to confirm linking address with wrong jwt', async () => {
+		const signature = 'fake';
+		fakectx.req.headers.authorization = 'Bearer wrong';
+		try {
+			await addressLinkConfirm(null, { signature, address_id: 1 }, fakectx);
+		} catch (error) {
+			expect(error).to.exist;
+			expect(error).to.be.an.instanceof(AuthenticationError);
+			expect(error.message).to.eq(messages.INVALID_JWT);
+		}
+	});
+});
---
+++ auth-server/test/resolvers/mutation/addressLinkStart.spec.ts
@@ -0,0 +1,71 @@
+import { expect } from 'chai';
+import { AuthenticationError } from 'apollo-server';
+import 'mocha';
+
+import User from '../../../src/model/User';
+import Address from '../../../src/model/Address';
+import addressLinkStart from '../../../src/resolvers/mutation/addressLinkStart';
+import signup from '../../../src/resolvers/mutation/signup';
+import { Context } from '../../../src/types';
+import messages from '../../../src/utils/messages';
+
+describe('addressLinkStart mutation', () => {
+	let signupResult;
+	const fakectx: Context = {
+		req: {
+			headers: {}
+		},
+		res: {
+			cookie: () => {}
+		}
+	} as any;
+	const email = 'test@email.com';
+	const password = 'testpass';
+	const username = 'testuser';
+	const name = 'test name';
+
+	before(async () => {
+		signupResult = await signup(null, { email, password, username, name }, fakectx);
+		fakectx.req.headers.authorization = `Bearer ${signupResult.token}` // eslint-disable-line
+	});
+
+	after(async () => {
+		await User
+			.query()
+			.where({ id: signupResult.user.id })
+			.del();
+	});
+
+	it('should be able to start linking address', async () => {
+		const network = 'kasuma';
+		const address = 'HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F'; // Alice
+		const res = await addressLinkStart(null, { network, address }, fakectx);
+
+		const dbAddress = await Address
+			.query()
+			.where({ user_id: signupResult.user.id })
+			.first();
+
+		expect(dbAddress.network).to.be.equal(network);
+		expect(dbAddress.address).to.be.equal(address);
+		expect(dbAddress.sign_message).to.exist;
+		expect(dbAddress.sign_message).to.be.a('string');
+		expect(dbAddress.sign_message).to.equal(res.sign_message);
+		expect(dbAddress.verified).to.be.false;
+
+		expect(res.message).to.equal(messages.ADDRESS_LINKING_STARTED);
+	});
+
+	it('should not be able to start linking address with wrong jwt', async () => {
+		const network = 'kasuma';
+		const address = 'HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F'; // Alice
+		fakectx.req.headers.authorization = 'Bearer wrong';
+		try {
+			await addressLinkStart(null, { network, address }, fakectx);
+		} catch (error) {
+			expect(error).to.exist;
+			expect(error).to.be.an.instanceof(AuthenticationError);
+			expect(error.message).to.eq(messages.INVALID_JWT);
+		}
+	});
+});
