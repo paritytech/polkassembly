46acfb54eb3d4682ba82c45a3820af01fa78e772
Nikhil Ranjan <niklabh811@gmail.com>
2020-02-05 18:24:08 +0530

    removed process.env.REACT_APP_AUTH_SERVER_GRAPHQL_URL

--- front-end/src/components/Apollo.tsx
+++ front-end/src/components/Apollo.tsx
@@ -22,7 +22,7 @@ import { JWTPayploadType } from '../types';
  * to get a new jwt token.
  */
 const fetchAccessToken = () => (
-	fetch(`${process.env.REACT_APP_AUTH_SERVER_GRAPHQL_URL}`, {
+	fetch(`${process.env.REACT_APP_HASURA_GRAPHQL_URL}`, {
 		body: JSON.stringify({ 'operationName':null,'query':'query get_new_token { token { token }}' }),
 		credentials: 'same-origin',
 		headers: {
