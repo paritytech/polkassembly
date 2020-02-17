01dda9e45a46cb0095ba2092e6b22915337498af
Michel Erler <7072141+erler@users.noreply.github.com>
2020-02-17 15:54:35 +0100

    T&C and Privacy Policy pages, basic footer (#324)
    * screens
    
    * sign up links
    
    * basic footer
    
    * container
    
    * checkbox validation
    
    * checkbox styling
    
    * temp app container fix
    
    * linting
    
    * basic mobile footer
    
    * removing containers
    
    * Update front-end/src/screens/SignupForm/index.tsx
    
    Co-Authored-By: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * Update front-end/src/App.tsx
    
    Co-Authored-By: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * Update front-end/src/screens/Footer/index.tsx
    
    Co-Authored-By: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * md as ts
    
    * Delete GlobalStyle.tsx
    
    changed in #347
    
    * Create GlobalStyle.tsx
    
    * Footer (#347)
    
    * footer at bottom
    
    * smaller footer
    
    * Update front-end/src/ui-components/GlobalStyle.tsx
    
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * Update front-end/src/screens/SignupForm/index.tsx
    
    Co-Authored-By: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * Update front-end/src/screens/SignupForm/index.tsx
    
    Co-Authored-By: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * checkbox adjustments
    
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>

---
+++ front-end/src/assets/privacy-policy.ts
@@ -0,0 +1,4 @@
+export const privacyPolicyMd = `# Privacy Policy
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
