11ff754b3387a7885eb2a07a82e815e70acc9826
thib <thibaut.sardan@gmail.com>
2020-02-21 13:24:06 +0100

    init

---
+++ front-end/src/components/Post/GovernanceSideBar/VoteMotion.tsx
@@ -0,0 +1,191 @@
+
+import React, { useContext, useState } from 'react';
+import styled from '@xstyled/styled-components';
+import { /* Divider, */ Dropdown, DropdownProps, DropdownItemProps, Icon, Popup, Select } from 'semantic-ui-react';
+import { ApiPromise } from '@polkadot/api';
+import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
+
+import { Form } from '../../../ui-components/Form';
+import Button from '../../../ui-components/Button';
+import { NotificationContext } from '../../../context/NotificationContext';
+import { NotificationStatus } from '../../../types';
+
+interface Props {
+	className?: string
+	motionId?: number | null | undefined
+	api?: ApiPromise
+	apiReady?: boolean
+	address: string
+	defaultAddress: string
+	addressOptions: DropdownItemProps[]
+	accounts: InjectedAccountWithMeta[]
+	onAccountChange: (event: React.SyntheticEvent<HTMLElement, Event>, data: DropdownProps) => void
+	getAccounts: () => Promise<undefined>
+}
+
+const VoteMotion = ({ className, motionId, api, apiReady, address, defaultAddress, accounts, addressOptions, onAccountChange, getAccounts }: Props) => {
+	const { queueNotification } = useContext(NotificationContext);
+
+	const voteMotion = async (aye: boolean) => {
+		if (!api) {
+			console.error('polkadot/api not set');
+			return;
+		}
+
+		if (!motionId) {
+			console.error('motionId not set');
+			return;
+		}
+
+		const vote = api.tx.council.vote('',motionId, aye);
+
+		vote.signAndSend(address, ({ status }) => {
+			if (status.isFinalized) {
+				queueNotification({
+					header: 'Success!',
+					message: `Vote on motion #${motionId} successfully finalized`,
+					status: NotificationStatus.SUCCESS
+				});
+				console.log(`Completed at block hash #${status.asFinalized.toString()}`);
+			} else {
+				console.log(`Current status: ${status.type}`);
+			}
+		}).catch((error) => {
+			console.log(':( transaction failed');
+			console.error('ERROR:', error);
+			queueNotification({
+				header: 'Failed!',
+				message: error.message,
+				status: NotificationStatus.ERROR
+			});
+		});
+	};
+
+	if (accounts.length === 0) {
+		return (
+			<div className={className}>
+				<div className='card'>
+					<Form standalone={false}>
+						<h4>Vote</h4>
+						<Form.Group>
+							<Form.Field className='button-container'>
+								<Button
+									primary
+									onClick={getAccounts}
+								>
+									Vote
+								</Button>
+							</Form.Field>
+						</Form.Group>
+					</Form>
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<div className={className}>
+			<div className='card'>
+				<Form standalone={false}>
+					<h4>Vote</h4>
+					<Form.Group>
+						<Form.Field width={16}>
+							<label>Vote with account
+								<Popup
+									trigger={<Icon name='question circle'/>}
+									content='You can choose account from polkadot-js extension.'
+									style={{ fontSize: '1.2rem', marginLeft: '-1rem' }}
+									hoverable={true}
+								/>
+							</label>
+							<Dropdown
+								onChange={onAccountChange}
+								defaultValue={defaultAddress || accounts[0].address}
+								selection
+								options={addressOptions}
+							/>
+							<label>Vote Lock&nbsp;
+								<Popup
+									trigger={<Icon name='question circle'/>}
+									content='You can multiply your votes by locking your tokens for longer periods of time.'
+									style={{ fontSize: '1.2rem', marginLeft: '-1rem' }}
+									hoverable={true}
+								/>
+							</label>
+						</Form.Field>
+					</Form.Group>
+					<Form.Group>
+						<Form.Field className='button-container' width={8}>
+							<Button
+								fluid
+								basic
+								className='primary negative'
+								disabled={!apiReady}
+								onClick={() => voteMotion(false)}
+							>
+								<Icon name='thumbs down' />
+								NAY
+							</Button>
+						</Form.Field>
+						<Form.Field className='button-container' width={8}>
+							<Button
+								fluid
+								primary
+								disabled={!apiReady}
+								onClick={() => voteMotion(true)}
+							>
+								<Icon name='thumbs up' />
+								AYE
+							</Button>
+						</Form.Field>
+					</Form.Group>
+				</Form>
+			</div>
+		</div>
+	);
+};
+
+export default styled(VoteMotion)`
+	.card {
+		background-color: white;
+		padding: 2rem 3rem 4rem 3rem;
+		border-style: solid;
+		border-width: 1px;
+		border-color: grey_light;
+		margin-bottom: 1rem;
+		@media only screen and (max-width: 768px) {
+			padding: 2rem;
+		}
+	}
+
+	.ui.selection.dropdown {
+		border-radius: 0rem;
+	}
+
+	.ui.dropdown .menu .active.item {
+		font-weight: 500;
+	}
+
+	.ui.dropdown .menu>.item:hover {
+		background-color: grey_light;
+	}
+
+	.ui.selection.dropdown:focus, .ui.selection.active.dropdown, .ui.selection.active.dropdown:hover, .ui.selection.active.dropdown .menu {
+		border-color: grey_light;
+	}
+
+	i.icon.question.circle:before {
+		color: grey_secondary;
+	}
+
+	@media only screen and (max-width: 768px) {
+		.ui.form {
+			padding: 0rem;
+		}
+
+		.button-container {
+			margin-bottom: 1rem!important;
+		}
+	}
+
+`;
