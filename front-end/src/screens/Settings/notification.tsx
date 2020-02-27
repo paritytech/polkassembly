ce56d286f5f830cbe2babe4e33119eba49ea747b
Nikhil Ranjan <niklabh811@gmail.com>
2020-02-27 17:11:02 +0530

    ugly code for notification control center

--- front-end/src/screens/Settings/notification.tsx
+++ front-end/src/screens/Settings/notification.tsx
@@ -1,42 +1,36 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Checkbox, CheckboxProps } from 'semantic-ui-react';
 
 import { NotificationContext } from '../../context/NotificationContext';
 import { useChangeNotificationPreferenceMutation, useNotificationQuery } from '../../generated/graphql';
 import { NotificationStatus } from '../../types';
 import FilteredError from '../../ui-components/FilteredError';
+import Button from '../../ui-components/Button';
 import { Form } from '../../ui-components/Form';
 
 const Notification = (): JSX.Element => {
-	const [post_participated, set_post_participated] = useState(false);
-	const [post_created, set_post_created] = useState(false);
-	const [new_proposal, set_new_proposal] = useState(false);
-	const [own_proposal, set_own_proposal] = useState(false);
+	const [post_participated, set_post_participated] = useState<boolean>(false);
+	const [post_created, set_post_created] = useState<boolean>(false);
+	const [new_proposal, set_new_proposal] = useState<boolean>(false);
+	const [own_proposal, set_own_proposal] = useState<boolean>(false);
 	const [changeNotificationPreferenceMutation, { error }] = useChangeNotificationPreferenceMutation();
+	const { data } = useNotificationQuery();
 	const { queueNotification } = useContext(NotificationContext);
 
-
 	useEffect(() => {
-		const { data } = useNotificationQuery();
+		set_post_participated(data?.notification?.post_participated || false);
+		set_post_created(data?.notification?.post_created || false);
+		set_new_proposal(data?.notification?.new_proposal || false);
+		set_own_proposal(data?.notification?.own_proposal || false);
+	}, [data]);
 
-	}, []);
-
-
-
-	console.log(data);
-
-	const handleOnChange = (event: React.FormEvent<HTMLInputElement>, data: CheckboxProps) => {
-		console.log(event, data);
-
-		const value = data.value?.toString() || '';
-
-		if (!value) {
-			return;
-		}
-
+	const updatePreference = () => {
 		changeNotificationPreferenceMutation({
 			variables: {
-				[value]: data.checked
+				new_proposal,
+				own_proposal,
+				post_created,
+				post_participated
 			}
 		})
 			.then(({ data }) => {
@@ -57,6 +51,25 @@ const Notification = (): JSX.Element => {
 			});
 	};
 
+	const handlePostParticipatedChange = (event: React.FormEvent<HTMLInputElement>, data: CheckboxProps) => {
+		set_post_participated(data.checked || false);
+	};
+
+	const handlePostCreatedChange = (event: React.FormEvent<HTMLInputElement>, data: CheckboxProps) => {
+		set_post_created(data.checked || false);
+		updatePreference();
+	};
+
+	const handleNewProposalChange = (event: React.FormEvent<HTMLInputElement>, data: CheckboxProps) => {
+		set_new_proposal(data.checked || false);
+		updatePreference();
+	};
+
+	const handleOwnProposalChange = (event: React.FormEvent<HTMLInputElement>, data: CheckboxProps) => {
+		set_own_proposal(data.checked || false);
+		updatePreference();
+	};
+
 	return (
 		<Form standalone={false}>
 			<Form.Group>
@@ -66,29 +79,38 @@ const Notification = (): JSX.Element => {
 			</Form.Group>
 			<Form.Group>
 				<Form.Field>
-					<Checkbox value='post_participated' label='Subscribe to post you participate in' toggle onChange={handleOnChange} />
+					<Checkbox label='Subscribe to post you participate in' checked={post_participated} toggle onChange={handlePostParticipatedChange} />
 				</Form.Field>
 			</Form.Group>
 			<Form.Group>
 				<Form.Field>
-					<Checkbox value='post_created' label='Subscribe to post you created' toggle />
+					<Checkbox label='Subscribe to post you created' checked={post_created} toggle onChange={handlePostCreatedChange} />
 				</Form.Field>
 			</Form.Group>
 			<Form.Group>
 				<Form.Field>
-					<Checkbox value='new_proposal' label='Notified for new proposal in council/motion/referendum' toggle />
+					<Checkbox label='Notified for new proposal in council/motion/referendum' checked={new_proposal} toggle onChange={handleNewProposalChange} />
 				</Form.Field>
 			</Form.Group>
 			<Form.Group>
 				<Form.Field>
-					<Checkbox value='own_proposal' label='Notified for your own proposals' toggle />
+					<Checkbox label='Notified for your own proposals' checked={own_proposal} toggle onChange={handleOwnProposalChange} />
 				</Form.Field>
 			</Form.Group>
 			<Form.Group>
-				<Form.Field>
+				<Form.Field width={6}>
+					<label>&nbsp;</label>
 					{error && <FilteredError text={error.message}/>}
+					<Button
+						secondary
+						onClick={updatePreference}
+						type="submit"
+					>
+					Change Preference
+					</Button>
 				</Form.Field>
 			</Form.Group>
+
 		</Form>
 	);
 };
