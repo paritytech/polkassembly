6cfce8756f8c79fd614b29bdf58395e2d28d9343
erler <mail@michelerler.com>
2020-03-02 13:42:42 +0100

    Onchain info style wrapper

--- front-end/src/components/Post/PostGovernanceInfo/PostProposalInfo.tsx
+++ front-end/src/components/Post/PostGovernanceInfo/PostProposalInfo.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { Grid } from 'semantic-ui-react';
-import styled from '@xstyled/styled-components';
 
 import AddressComponent from '../../../ui-components/Address';
+import OnchainInfoStyle from '../../../ui-components/OnchainInfoStyle';
 import { OnchainLinkProposalFragment } from '../../../generated/graphql';
 import { chainProperties } from '../../../global/networkConstants';
 import getNetwork from '../../../util/getNetwork';
@@ -27,85 +27,47 @@ const PostProposalInfo = ({ className, onchainLink }: Props) => {
 	const { metaDescription, method, preimageArguments } = preimage || {};
 
 	return (
-		<div className={className}>
+		<OnchainInfoStyle className={className}>
 			<h4>On-Chain Info</h4>
 			<Grid>
 				<Grid.Column mobile={16} tablet={8} computer={8}>
-					<div className='info_group'>
-						<h6>Proposer</h6>
-						<AddressComponent className='' address={proposerAddress} accountName={'Proposer Address'} />
-					</div>
+					<h6>Proposer</h6>
+					<AddressComponent className='' address={proposerAddress} accountName={'Proposer Address'} />
 				</Grid.Column>
 				{depositAmount && currentNetwork &&
 				<Grid.Column mobile={16} tablet={8} computer={8}>
-					<div className='info_group'>
-						<h6>Deposit</h6>
-						{parseInt(depositAmount) / Math.pow(10, chainProperties[currentNetwork].tokenDecimals) + ' ' + chainProperties[currentNetwork].tokenSymbol}
-					</div>
+					<h6>Deposit</h6>
+					{parseInt(depositAmount) / Math.pow(10, chainProperties[currentNetwork].tokenDecimals) + ' ' + chainProperties[currentNetwork].tokenSymbol}
 				</Grid.Column>}
 				{method &&
 				<Grid.Row>
 					<Grid.Column mobile={16} tablet={8} computer={8}>
-						<div className='info_group'>
-							<h6>Method</h6>
-							{method}
-						</div>
+						<h6>Method</h6>
+						{method}
 					</Grid.Column>
 					<Grid.Column mobile={16} tablet={8} computer={8}>
 						{preimageArguments && preimageArguments.length
-							? <div className='info_group'>
+							? <>
 								<h6>Arguments</h6>
 								{preimageArguments.map((element, index) => {
 									return <div className={'methodArguments'} key={index}>
 										<span key={index}>{element.name}: {element.value}</span>
 									</div>;
 								})}
-							</div>
+							</>
 							: null}
 					</Grid.Column>
 				</Grid.Row>}
 				<Grid.Column mobile={16} tablet={16} computer={16}>
 					{ metaDescription &&
-					<div className='info_group'>
+					<>
 						<h6>Description</h6>
 						{metaDescription}
-					</div>}
+					</>}
 				</Grid.Column>
 			</Grid>
-		</div>
+		</OnchainInfoStyle>
 	);
 };
 
-export default styled(PostProposalInfo)`
-	background-color: white;
-	padding: 2rem 3rem 2rem 3rem;
-	border-style: solid;
-	border-width: 5px;
-	border-color: grey_light;
-	font-size: sm;
-	overflow-wrap: break-word;
-	margin-bottom: 1rem;
-	font-family: font_mono;
-
-	h6 {
-		font-family: font_mono;
-		font-size: sm;
-	}
-
-	h4 {
-		font-size: lg;
-		font-family: font_mono;
-		margin-bottom: 2rem;
-	}
-
-	.methodArguments {
-		display: inline-block;
-		overflow-x: auto;
-		width: 100%;
-		word-wrap: normal;
-	}
-
-	@media only screen and (max-width: 576px) {
-		padding: 2rem;
-	}
-`;
+export default PostProposalInfo;
--- front-end/src/components/Post/PostGovernanceInfo/PostReferendumInfo.tsx
+++ front-end/src/components/Post/PostGovernanceInfo/PostReferendumInfo.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { Grid } from 'semantic-ui-react';
-import styled from '@xstyled/styled-components';
 
 import AddressComponent from '../../../ui-components/Address';
+import OnchainInfoStyle from '../../../ui-components/OnchainInfoStyle';
 import { OnchainLinkReferendumFragment } from '../../../generated/graphql';
 
 interface Props{
@@ -26,99 +26,55 @@ const PostReferendumInfo = ({ className, onchainLink }: Props) => {
 	const { metaDescription, method, preimageArguments } = preimage || {};
 
 	return (
-		<div className={className}>
+		<OnchainInfoStyle className={className}>
 			<h4>On-Chain Info</h4>
 			<Grid>
 				<Grid.Column mobile={16} tablet={8} computer={8}>
-					<div className='info_group'>
-						<h6>Proposer</h6>
-						<AddressComponent className='' address={proposerAddress} accountName={'Proposer Address'}/>
-					</div>
+					<h6>Proposer</h6>
+					<AddressComponent className='' address={proposerAddress} accountName={'Proposer Address'}/>
 				</Grid.Column>
 				{delay &&
 					<Grid.Column mobile={16} tablet={8} computer={8}>
-						<div className='info_group'>
-							<h6>Delay</h6>
-							{delay}
-						</div>
+						<h6>Delay</h6>
+						{delay}
 					</Grid.Column>}
 				{end &&
 					<Grid.Column mobile={16} tablet={8} computer={8}>
-						<div className='info_group'>
-							<h6>End</h6>
-							{end}
-						</div>
+						<h6>End</h6>
+						{end}
 					</Grid.Column>}
 				{voteThreshold &&
 					<Grid.Column mobile={16} tablet={8} computer={8}>
-						<div className='info_group'>
-							<h6>voteThreshold</h6>
-							{voteThreshold}
-						</div>
+						<h6>voteThreshold</h6>
+						{voteThreshold}
 					</Grid.Column>}
 				{method &&
 				<Grid.Row>
 					<Grid.Column mobile={16} tablet={8} computer={8}>
-						<div className='info_group'>
-							<h6>Method</h6>
-							{method}
-						</div>
+						<h6>Method</h6>
+						{method}
 					</Grid.Column>
 					<Grid.Column mobile={16} tablet={8} computer={8}>
 						{preimageArguments && preimageArguments.length
-							? <div className='info_group'>
+							? <>
 								<h6>Arguments</h6>
 								{preimageArguments.map((element, index) => {
 									return <div className={'methodArguments'} key={index}>
 										<span key={index}>{element.name}: {element.value}</span>
 									</div>;
 								})}
-							</div>
+							</>
 							: null}
 					</Grid.Column>
 				</Grid.Row>}
 				{metaDescription &&
 				<Grid.Column mobile={16} tablet={16} computer={16}>
-					<div className='info_group'>
-						<h6>Description</h6>
-						{metaDescription}
-					</div>
+					<h6>Description</h6>
+					{metaDescription}
 				</Grid.Column>}
 			</Grid>
-		</div>
+		</OnchainInfoStyle>
 	);
 };
 
-export default styled(PostReferendumInfo)`
-	background-color: white;
-	padding: 2rem 3rem 2rem 3rem;
-	border-style: solid;
-	border-width: 5px;
-	border-color: grey_light;
-	font-size: sm;
-	overflow-wrap: break-word;
-	margin-bottom: 1rem;
-	font-family: font_mono;
-
-	h6 {
-		font-family: font_mono;
-		font-size: sm;
-	}
-
-	h4 {
-		font-size: lg;
-		font-family: font_mono;
-		margin-bottom: 2rem;
-	}
-
-	.methodArguments {
-		display: inline-block;
-		overflow-x: auto;
-		width: 100%;
-		word-wrap: normal;
-	}
-
-	@media only screen and (max-width: 576px) {
-		padding: 2rem;
-	}
-`;
+export default PostReferendumInfo;
