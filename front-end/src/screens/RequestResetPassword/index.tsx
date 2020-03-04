34800dac7d09d58d343d62ba1ce07eb52094f4c7
Nikhil Ranjan <niklabh811@gmail.com>
2020-03-04 21:57:56 +0530

    Fix reset password (#462)
    * Fix reset password
    
    * largeScreen width
    
    Co-authored-by: Michel Erler <7072141+erler@users.noreply.github.com>

--- front-end/src/screens/RequestResetPassword/index.tsx
+++ front-end/src/screens/RequestResetPassword/index.tsx
@@ -43,10 +43,10 @@ const RequestResetPassword = ({ className }:Props): JSX.Element => {
 
 	return (
 		<Grid className={className}>
-			<Grid.Column only='tablet computer' tablet={2} computer={4} largeScreen={5} widescreen={6}/>
-			<Grid.Column mobile={16} tablet={12} computer={8} largeScreen={6} widescreen={4}>
+			<Grid.Column only='tablet computer' tablet={2} computer={4} largeScreen={5} widescreen={5}/>
+			<Grid.Column mobile={16} tablet={12} computer={8} largeScreen={6} widescreen={6}>
 				<Form>
-					<h3>Request password reset</h3>
+					<h3>Request Password Reset</h3>
 					<Form.Group>
 						<Form.Field width={16}>
 							<label>Email</label>
@@ -71,7 +71,7 @@ const RequestResetPassword = ({ className }:Props): JSX.Element => {
 					</div>
 				</Form>
 			</Grid.Column>
-			<Grid.Column only='tablet computer' tablet={2} computer={4} largeScreen={5} widescreen={6}/>
+			<Grid.Column only='tablet computer' tablet={2} computer={4} largeScreen={5} widescreen={5}/>
 		</Grid>
 	);
 };
