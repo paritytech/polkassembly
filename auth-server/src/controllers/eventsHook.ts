bfaf0580f8fc22663134f41136b2f37368e2c4c6
Nikhil Ranjan <niklabh811@gmail.com>
2020-03-04 21:12:17 +0530

    Added new proposal email

--- auth-server/src/controllers/eventsHook.ts
+++ auth-server/src/controllers/eventsHook.ts
@@ -4,12 +4,12 @@ import Address from '../model/Address';
 import Notification from '../model/Notification';
 import PostSubscription from '../model/PostSubscription';
 import User from '../model/User';
-import { sendPostSubscriptionMail, sendProposalCreatedEmail } from '../services/email';
+import { sendPostSubscriptionMail, sendOwnProposalCreatedEmail, sendNewProposalCreatedEmail } from '../services/email';
 import getUserFromUserId from '../utils/getUserFromUserId';
 import messages from '../utils/messages';
 import { MessageType } from '../types';
 
-const sendPostCommentSubscriptionMail = async (comment): Promise<MessageType> => {
+const sendPostCommentSubscription = async (comment): Promise<MessageType> => {
 	const { post_id, author_id } = comment;
 
 	if (!post_id) {
@@ -48,13 +48,14 @@ const sendPostCommentSubscriptionMail = async (comment): Promise<MessageType> =>
 	return { message: messages.EVENT_POST_SUBSCRIPTION_MAIL_SENT };
 };
 
-const sendProposalCreatedMail = async (onchainLink): Promise<MessageType> => {
+const sendOwnProposalCreated = async (onchainLink): Promise<MessageType> => {
 	const {
 		proposer_address,
 		post_id,
 		onchain_motion_id,
 		onchain_proposal_id,
-		onchain_referendum_id
+		onchain_referendum_id,
+		onchain_treasury_proposal_id
 	} = onchainLink;
 
 	if (!proposer_address) {
@@ -121,11 +122,75 @@ const sendProposalCreatedMail = async (onchainLink): Promise<MessageType> => {
 		link = 'referendum';
 	}
 
+	if (onchain_treasury_proposal_id === 0 || onchain_treasury_proposal_id) {
+		link = 'treasury';
+	}
+
 	link += `/${post_id}`;
 
-	sendProposalCreatedEmail(user, link);
+	sendOwnProposalCreatedEmail(user, link);
 
 	return { message: messages.EVENT_PROPOSAL_CREATED_MAIL_SENT };
+};
+
+const sendNewProposalCreated = async (onchainLink) => {
+	const {
+		post_id,
+		onchain_motion_id,
+		onchain_proposal_id,
+		onchain_referendum_id,
+		onchain_treasury_proposal_id
+	} = onchainLink;
+
+	if (!post_id) {
+		return { message: messages.EVENT_POST_ID_NOT_FOUND } ;
+	}
+
+	const notifications = await Notification
+		.query()
+		.where({
+			new_proposal: true
+		});
+
+	const promises = notifications.map(async notification => {
+		const user = await User
+			.query()
+			.findById(notification.user_id);
+
+		if (!user) {
+			return { message: messages.EVENT_USER_NOT_FOUND };
+		}
+
+		if (!user.email) {
+			return { message: messages.EVENT_USER_EMAIL_NOT_FOUND };
+		}
+
+		if (!user.email_verified) {
+			return { message: messages.EVENT_USER_EMAIL_NOT_VERIFIED };
+		}
+
+		let type = '';
+
+		if (onchain_motion_id === 0 || onchain_motion_id) {
+			type = 'motion';
+		}
+
+		if (onchain_proposal_id === 0 || onchain_proposal_id) {
+			type = 'proposal';
+		}
+
+		if (onchain_referendum_id === 0 || onchain_referendum_id) {
+			type = 'referendum';
+		}
+
+		if (onchain_treasury_proposal_id === 0 || onchain_treasury_proposal_id) {
+			type = 'treasury';
+		}
+
+		sendNewProposalCreatedEmail(user, type, post_id);
+	});
+
+	Promise.all(promises).catch(error => console.error(error));
 };
 
 export const commentCreateHook = async (req: Request, res: Response) => {
@@ -135,7 +200,7 @@ export const commentCreateHook = async (req: Request, res: Response) => {
 
 	const comment = req.body?.event?.data?.new || {};
 
-	const result = await sendPostCommentSubscriptionMail(comment);
+	const result = await sendPostCommentSubscription(comment);
 
 	res.json(result);
 };
@@ -147,7 +212,10 @@ export const onchainLinksCreateHook = async (req: Request, res: Response) => {
 
 	const onchainLink = req.body?.event?.data?.new || {};
 
-	const result = await sendProposalCreatedMail(onchainLink);
+	const result = await sendOwnProposalCreated(onchainLink);
+
+	// Doing this in background as several emails needs to be sent
+	sendNewProposalCreated(onchainLink);
 
 	res.json(result);
 };
