74bd41223d3988f4d87a0616a2bc7c99e09145d6
Nikhil Ranjan <niklabh811@gmail.com>
2020-03-05 15:37:21 +0530

    Update auth-server/src/utils/messages.ts
    Co-Authored-By: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>

--- auth-server/src/utils/messages.ts
+++ auth-server/src/utils/messages.ts
@@ -34,7 +34,7 @@ export default {
 	JWT_REFRESH_TOKEN_USER_MISMATCH: 'User don\'t match between the JWT and the refresh-token.',
 	LOGOUT_SUCCESSFUL: 'Successfully logged out.',
 	NAME_CHANGED_SUCCESSFULLY: 'Name changed succefully.',
-	NAME_LENGTH_ERROR: 'Name cannot be larger than 255 characters.',
+	NAME_LENGTH_ERROR: 'Name cannot be larger than 30 characters.',
 	NO_CORRESPONDING_REFRESH_TOKEN: 'No corresponding refresh-token found.',
 	NO_USER_FOUND_WITH_USERNAME: 'No user found with this username',
 	NOTIFICATION_PREFERENCE_CHANGE_SUCCESSFUL: 'Notification preference changed successfully.',
