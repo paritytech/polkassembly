9d667577f95d275bd71c532aa4a239bbf13cb426
thib <thibaut.sardan@gmail.com>
2020-03-05 16:34:24 +0100

    nit to makes things clear

--- front-end/src/util/messages.ts
+++ front-end/src/util/messages.ts
@@ -2,12 +2,12 @@ export default {
 	VALIDATION_CONTENT_ERROR: 'Did you forget to add content?',
 	VALIDATION_EMAIL_ERROR: 'You need to provide a valid email!',
 	VALIDATION_NAME_MAXLENGTH_ERROR: 'Display name cannot be larger than 30 characters.',
-	VALIDATION_NAME_MINLENGTH_ERROR: 'Name should be longer that 2 characters.',
+	VALIDATION_NAME_MINLENGTH_ERROR: 'Name should be at least 3 characters long.',
 	VALIDATION_NAME_PATTERN_ERROR: 'Invalid character found, you can use letters, numbers, spaces and the following characters ._-',
 	VALIDATION_PASSWORD_ERROR: 'You need to specify a password with at least 6 characters!',
 	VALIDATION_TITLE_ERROR: 'Did you forget to add a title?',
 	VALIDATION_USERNAME_MAXLENGTH_ERROR: 'Username cannot be larger than 30 characters.',
-	VALIDATION_USERNAME_MINLENGTH_ERROR: 'Username should be longer that 2 characters.',
+	VALIDATION_USERNAME_MINLENGTH_ERROR: 'Username should be at least 3 characters long.',
 	VALIDATION_USERNAME_PATTERN_ERROR: 'Invalid character found, you can use letters, numbers and the following characters ._-',
 	VALIDATION_USERNAME_REQUIRED_ERROR: 'Username is mandatory.'
 };
