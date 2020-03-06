15fab2ad4411c6c486ed0935dd5f81003f694ded
thib <thibaut.sardan@gmail.com>
2020-03-06 11:07:40 +0100

    10s more before hasura launch

--- launch.sh
+++ launch.sh
@@ -21,7 +21,7 @@ sleep 5
 printf "\n\n-- migrating auth-server\n" 
 cd auth-server && yarn migrate &> auth-migration.log &
 
-sleep 5
+sleep 15
 
 printf "\n\n-- launching hasura docker\n"
 cd hasura && docker-compose up -d && cd ..;
@@ -31,8 +31,8 @@ sleep 5
 printf "\n\n-- hasura migration\n"
 cd hasura/hasura-migrations/migrations && hasura-dev migrate apply --admin-secret ip7itpC5L6R3RbNE4vkdZfsN && cd ../../..;
 
-printf "\n\n-- hasura reload metadata\n"
-cd hasura/hasura-migrations && hasura-dev metadata reload --admin-secret ip7itpC5L6R3RbNE4vkdZfsN && cd ../..;
+# printf "\n\n-- hasura reload metadata\n"
+# cd hasura/hasura-migrations && hasura-dev metadata reload --admin-secret ip7itpC5L6R3RbNE4vkdZfsN && cd ../..;
 
 printf "\n\n-- launching front-end\n"
 cd front-end && yarn && yarn start &> front-end.log &
