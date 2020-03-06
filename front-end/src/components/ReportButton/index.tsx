bdf052d4c9da7aae255eaa5f3fc32595288c52c9
Nikhil Ranjan <niklabh811@gmail.com>
2020-03-06 16:27:00 +0530

    Added form

--- front-end/src/components/ReportButton/index.tsx
+++ front-end/src/components/ReportButton/index.tsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Icon } from 'semantic-ui-react';
 
 import { useReportContentMutation } from '../../generated/graphql';
 import { NotificationContext } from '../../context/NotificationContext';
 import { NotificationStatus } from '../../types';
 import Button from '../../ui-components/Button';
+import Modal from '../../ui-components/Modal';
+import { Form } from '../../ui-components/Form';
+import { TextArea } from '../../ui-components/TextArea';
 
 interface DiscussionProps {
 	type: string
@@ -12,6 +15,7 @@ interface DiscussionProps {
 }
 
 const ReportButton = function ({ type, contentId }:DiscussionProps) {
+	const [showModal, setShowModal] = useState(false);
 	const [reason, setReason] = useState('');
 	const [comments, setComments] = useState('');
 	const [reportContentMutation] = useReportContentMutation();
@@ -38,13 +42,65 @@ const ReportButton = function ({ type, contentId }:DiscussionProps) {
 			.catch((e) => console.error('Error reporting content',e));
 	};
 
+	const openModal = () => {
+		setShowModal(true);
+	};
+	const dismissModal = () => {
+		setShowModal(false);
+	};
+	const onReasonChange = (event: React.ChangeEvent<HTMLInputElement>) => setReason(event.currentTarget.value);
+	const onCommentsChange = (value: string) => setComments(value);
+
 	return (
-		<Button
-			className={'social'}
-			onClick={handleReport}
-		>
-			<Icon name='flag'/>{' Report'}
-		</Button>
+		<>
+			{showModal ?
+				<Modal
+					buttons={<Button
+						content='Got it'
+						icon='check'
+						primary
+						onClick={dismissModal}
+					/>}
+					centered
+					dimmer='inverted'
+					open
+					onClose={dismissModal}
+					size='tiny'
+					title={'Nikhil'}
+				>
+					<Form standalone={false}>
+						<Form.Group>
+							<Form.Field width={10}>
+								<label>Reason</label>
+								<input
+									value={reason || ''}
+									onChange={onReasonChange}
+									placeholder='reason'
+									type='text'
+								/>
+							</Form.Field>
+						</Form.Group>
+						<Form.Group>
+							<Form.Field width={10}>
+								<label>Comments</label>
+								<TextArea
+									name={'comments'}
+									onChange={onCommentsChange}
+									value={comments || ''}
+								/>
+							</Form.Field>
+						</Form.Group>
+					</Form>
+				</Modal>
+				: null
+			}
+			<Button
+				className={'social'}
+				onClick={openModal}
+			>
+				<Icon name='flag'/>{' Report'}
+			</Button>
+		</>
 	);
 };
 
