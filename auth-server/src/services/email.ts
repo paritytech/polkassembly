13e76463ecb24cc7c2c9bcb657789fdd7fc9aba3
Nikhil Ranjan <niklabh811@gmail.com>
2020-03-11 22:46:35 +0530

    using ejs templates for emails

--- auth-server/src/services/email.ts
+++ auth-server/src/services/email.ts
@@ -59,15 +59,15 @@ export const sendResetPasswordEmail = (user: User, token: PasswordResetToken) =>
 	}
 
 	const resetUrl = `${DOMAIN}/reset-password/${token.token}`;
-	const text = `
+	const template = `
 		<p>
-			Hi ${user.name || ''}!<br/><br/>
+			Hi <%= username %>!<br/><br/>
 
 			It looks like you need to reset your password.<br />
 			Your secret is safe with us, and this will be a breeze.<br /><br />
 
 			Go ahead and follow the link to reset your password:<br /><br />
-			<a href="${resetUrl}">Reset Your Password</a><br /><br />
+			<a href="<%= resetUrl %>">Reset Your Password</a><br /><br />
 
 			Just a heads up, to make sure your information is safe and secure, the link will expire after 24 hours.<br /><br />
 
@@ -76,6 +76,8 @@ export const sendResetPasswordEmail = (user: User, token: PasswordResetToken) =>
 			Polkassembly Team
 		</p>
 	`;
+
+	const text = ejs.render(template, { username: user.name || '', resetUrl });
 
 	const msg = {
 		to: user.email,
@@ -95,18 +97,25 @@ export const sendPostSubscriptionMail = (user: User, author: User, comment) => {
 		return;
 	}
 
-	const text = `
+	const template = `
 		<p>
-			Hi ${user.name || ''}!<br/><br/>
+			Hi <%= username %>!<br/><br/>
 
 			<br />
-			${author.username} has commented on a <a href="${DOMAIN}/post/${comment.post_id}">post you subscribed to</a>.<br /><br />
+			${author.username} has commented on a <a href="<%= domain >/post/<%= postId %>">post you subscribed to</a>.<br /><br />
 
-			comment: ${comment.content}<br />
+			comment: <% content %><br />
 
 			Polkassembly Team
 		</p>
 	`;
+
+	const text = ejs.render(template, {
+		username: user.name || '',
+		domain: DOMAIN,
+		postId: comment.post_id,
+		content: comment.content
+	});
 
 	const msg = {
 		to: user.email,
@@ -127,21 +136,28 @@ export const sendUndoEmailChangeEmail = (user: User, undoToken: UndoEmailChangeT
 	}
 
 	const undoUrl = `${DOMAIN}/undo-email-change/${undoToken.token}`;
-	const text = `
+	const template = `
 		<p>
-			Hi ${user.name || ''}!<br/><br/>
+			Hi  <%= username %>!<br/><br/>
 
-			Your email on polkassembly.io was changed to ${user.email}.<br />
+			Your email on polkassembly.io was changed to <%= userEmail %>.<br />
 			If you did the change, then everything is fine, you have nothing to do.<br /><br />
 
-			If you did not change your email and suspect that it is a malicious attempt, click on the following link to change your account email back to: ${undoToken.email}<br /><br />
-			<a href="${undoUrl}">Recover Your Email</a><br /><br />
+			If you did not change your email and suspect that it is a malicious attempt, click on the following link to change your account email back to: <%= undoEmail %><br /><br />
+			<a href="<%= undoUrl %>">Recover Your Email</a><br /><br />
 
 			This link is valid for 48 hours, past this time, you will not be able to use it to recover your email. If you did not have time to click it and are a victim of a malicious email change, please open an issue on https://github.com/paritytech/polkassembly/issues/new<br /><br />
 
 			Polkassembly Team
 		</p>
 	`;
+
+	const text = ejs.render(template, {
+		username: user.name || '',
+		userEmail: user.email,
+		undoEmail: undoToken.email,
+		undoUrl
+	});
 
 	const msg = {
 		to: undoToken.email,
@@ -161,18 +177,25 @@ export const sendOwnProposalCreatedEmail = (user: User, type: string, postId: nu
 		return;
 	}
 
-	const text = `
+	const postUrl = `${DOMAIN}/${type}/${postId}`;
+	const template = `
 		<p>
-			Hi ${user.name || ''}!<br/><br/>
+			Hi <%= username %>!<br/><br/>
 
 			You have submitted a motion/proposal on chain.<br />
-			Click on the following link to login to Polkassembly and edit the proposal/motion description and title: <a href="${DOMAIN}/${type}/${postId}">${DOMAIN}/${type}/${postId}</a>.<br /><br />
+			Click on the following link to login to Polkassembly and edit the proposal/motion description and title: <a href="<%= postUrl %>"><%= postUrl %></a>.<br /><br />
 
-			You can deactivate this notification in your notification control center: <a href="${DOMAIN}/notifications">${DOMAIN}/notifications</a>
+			You can deactivate this notification in your notification control center: <a href="<%= domain %>/notifications"><%= domain %>/notifications</a>
 
 			Polkassembly Team
 		</p>
 	`;
+
+	const text = ejs.render(template, {
+		domain: DOMAIN,
+		postUrl,
+		username: user.name || ''
+	});
 
 	const msg = {
 		to: user.email,
@@ -192,18 +215,26 @@ export const sendNewProposalCreatedEmail = (user: User, type: string, postId: nu
 		return;
 	}
 
-	const text = `
+	const postUrl = `${DOMAIN}/${type}/${postId}`;
+	const template = `
 		<p>
-			Hi ${user.name || ''}!<br/><br/>
+			Hi <%= username %>!<br/><br/>
 
-			There is a new ${type} on chain.<br />
-			Click on the following link to check it out: <a href="${DOMAIN}/${type}/${postId}">${DOMAIN}/${type}/${postId}</a>.<br /><br />
+			There is a new <%= type %> on chain.<br />
+			Click on the following link to check it out: <a href="<%= postUrl %>"><%= postUrl %></a>.<br /><br />
 
-			You can deactivate this notification in your notification control center: <a href="${DOMAIN}/notifications">${DOMAIN}/notifications</a>
+			You can deactivate this notification in your notification control center: <a href="<%= domain %>/notifications"><%= domain %>/notifications</a>
 
 			Polkassembly Team
 		</p>
 	`;
+
+	const text = ejs.render(template, {
+		domain: DOMAIN,
+		postUrl,
+		type,
+		username: user.name || ''
+	});
 
 	const msg = {
 		to: user.email,
@@ -223,18 +254,26 @@ export const sendReportContentEmail = (username: string, type: string, contentId
 		return;
 	}
 
-	const text = `
+	const template = `
 		<p>
 			Content Reported.<br />
-			Reporter: ${username}<br />
+			Reporter: <%= username %><br />
 			Reason:<br />
-			${reason} <br />
+			<%= reason %> <br />
 			Comments:<br />
-			${comments} <br />
-			type: ${type} <br />
-			id: ${contentId} <br />
+			<%= comments %> <br />
+			type: <%= type %> <br />
+			id: <%= contentId %> <br />
 		</p>
 	`;
+
+	const text = ejs.render(template, {
+		comments,
+		contentId,
+		reason,
+		type,
+		username
+	});
 
 	const msg = {
 		to: REPORT,
