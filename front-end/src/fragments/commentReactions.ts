e08faa9e6b91c5b675f05d97857ae33af33c26fc
Nikhil Ranjan <niklabh811@gmail.com>
2020-03-18 19:52:15 +0530

    reactions for comments

---
+++ front-end/src/fragments/commentReactions.ts
@@ -0,0 +1,22 @@
+// Copyright 2019-2020 @paritytech/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import gql from 'graphql-tag';
+
+export const commentReactionFields = gql`
+    fragment commentReactionFields on comment_reactions {
+        id
+        reactor {
+            id
+            name
+            username
+        }
+        reaction {
+            id
+            reaction
+        }
+        created_at
+        updated_at
+    }
+`;
