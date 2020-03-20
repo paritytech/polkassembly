c1b86e514dd51c095ed03b3329804caf568c59ca
Nikhil Ranjan <niklabh811@gmail.com>
2020-03-20 19:19:54 +0530

    Added no. of seconds to proposals

--- front-end/src/components/Post/GovernanceSideBar/SecondProposal.tsx
+++ front-end/src/components/Post/GovernanceSideBar/SecondProposal.tsx
@@ -2,10 +2,11 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React, { useContext, useState } from 'react';
-import { DropdownProps } from 'semantic-ui-react';
+import React, { useContext, useState, useEffect } from 'react';
+import { DropdownProps, Grid } from 'semantic-ui-react';
 import styled from '@xstyled/styled-components';
 import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
+import { formatBalance } from '@polkadot/util';
 
 import { Form } from '../../../ui-components/Form';
 import Button from '../../../ui-components/Button';
@@ -28,6 +29,31 @@ const SecondProposal = ({ className, proposalId, address, accounts, onAccountCha
 	const [loadingStatus, setLoadingStatus] = useState<LoadingStatusType>({ isLoading: false, message:'' });
 	const { queueNotification } = useContext(NotificationContext);
 	const { api, apiReady } = useContext(ApiContext);
+	const [seconds, setSeconds] = useState(0);
+	const [deposit, setDeposit] = useState('');
+
+	useEffect(() => {
+		async function getSeconds() {
+			if (!api) {
+				console.error('polkadot/api not set');
+				return;
+			}
+
+			if (!apiReady) {
+				console.error('api not ready');
+			}
+
+			const proposals = await api.derive.democracy.proposals();
+			proposals.forEach((proposal) => {
+				if (proposal.index.toNumber() === proposalId) {
+					setSeconds(proposal.seconds.length);
+					setDeposit(formatBalance(proposal.balance));
+				}
+			});
+		}
+
+		getSeconds().catch(console.error);
+	}, [api, apiReady]);
 
 	const secondProposal = async () => {
 		if (!api) {
@@ -77,6 +103,18 @@ const SecondProposal = ({ className, proposalId, address, accounts, onAccountCha
 				<div className='card'>
 					<Form standalone={false}>
 						<h4>Vote</h4>
+						<Grid className={'info'} columns={2} divided>
+							<Grid.Row>
+								<Grid.Column>
+									<div><b>Deposit Amount</b></div>
+									<div>{deposit}</div>
+								</Grid.Column>
+								<Grid.Column>
+									<div><b>Seconded By</b></div>
+									{seconds ? <div>{seconds} Addresses</div> : null}
+								</Grid.Column>
+							</Grid.Row>
+						</Grid>
 						<Form.Group>
 							<Form.Field className='button-container'>
 								<Button
@@ -135,6 +173,10 @@ export default styled(SecondProposal)`
 
 	.LoaderWrapper {
 		height: 15rem;
+	}
+
+	.info {
+		margin-bottom: 1rem;
 	}
 
 	@media only screen and (max-width: 768px) {
