eff38367909776d62225a8bfff679f6e24cc7420
Michel Erler <7072141+erler@users.noreply.github.com>
2020-04-06 09:11:37 +0200

    Method on GovernanceCard (#597)
    * method on govcard
    
    * mobile
    
    * spaces
    
    * removing redundant things
    
    * topic var to className

--- front-end/src/ui-components/StatusTag.tsx
+++ front-end/src/ui-components/StatusTag.tsx
@@ -60,38 +60,38 @@ export default styled(StatusTag).attrs(( { status }: Props) => ({
 	className: status,
 	content: status
 }))`
-    &.ui.label {
-        font-size: xs;
-        font-weight: 500;
-        background-color: white;
-        color: grey_primary;
-        border-style: solid;
-        border-width: 1px;
-        border-radius: 0.2rem;
+	&.ui.label {
+		font-size: xs;
+		font-weight: 500;
+		background-color: white;
+		color: grey_primary;
+		border-style: solid;
+		border-width: 1px;
+		border-radius: 0.2rem;
 		letter-spacing: 0.05rem;
 		text-transform: capitalize;
 		padding: 0.5rem 1rem;
-        &.${referendumStatus.STARTED},
+		&.${referendumStatus.STARTED},
 		&.${proposalStatus.PROPOSED},
 		&.${motionStatus.PROPOSED} {
-            border-color: blue_primary;
+			border-color: blue_primary;
 			color: blue_primary;
 			background-color: blue_primary_transparent;
-        }
-        &.${referendumStatus.PASSED},
+		}
+		&.${referendumStatus.PASSED},
 		&.${referendumStatus.EXECUTED},
 		&.${motionStatus.EXECUTED} {
-            border-color: green_primary;
+			border-color: green_primary;
 			color: green_primary;
 			background-color: green_primary_transparent;
-        }
-        &.${referendumStatus.CANCELLED},
+		}
+		&.${referendumStatus.CANCELLED},
 		&.${referendumStatus.NOTPASSED},
 		&.${referendumStatus.VETOED},
 		&.${motionStatus.DISAPPROVED} {
-            border-color: red_primary;
+			border-color: red_primary;
 			color: red_primary;
 			background-color: red_primary_transparent;
-        }
-    }
+		}
+	}
 `;
