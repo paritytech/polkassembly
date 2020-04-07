5a9c765650bd607e51d67cdb36f4839b5dd9fd2e
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2020-04-07 13:34:37 +0200

    Enforce headers in eslint (#611)
    * lint
    
    * Update auth-server/migrations/20191127103957_email_verification_token.js
    
    * Update auth-server/migrations/20200111182158_undo_email_change_token.js

--- auth-server/test/resolvers/mutation/requestResetPassword.spec.ts
+++ auth-server/test/resolvers/mutation/requestResetPassword.spec.ts
@@ -1,3 +1,6 @@
+// Copyright 2019-2020 @paritytech/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
 import { UserInputError, AuthenticationError } from 'apollo-server';
 import { expect } from 'chai';
 import { uuid } from 'uuidv4';
