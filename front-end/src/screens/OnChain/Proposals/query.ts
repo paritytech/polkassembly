4f07a51a40533dd73ccfb4129393d63e5c9baf37
thib <thibaut.sardan@gmail.com>
2020-04-08 11:46:29 +0200

    add id

--- front-end/src/screens/OnChain/Proposals/query.ts
+++ front-end/src/screens/OnChain/Proposals/query.ts
@@ -62,6 +62,7 @@ export const QUERY_LATEST_PROPOSALS = gql`
                     status
                 }
                 preimage {
+                    id
                     method
                 }
             }
