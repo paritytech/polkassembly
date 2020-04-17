2f040b686f80afe0823e0dee4628ccd9aff45e13
Nikhil Ranjan <niklabh811@gmail.com>
2020-04-17 19:23:38 +0530

    Added frontend validation to user name with react hook

--- front-end/src/screens/Settings/username.tsx
+++ front-end/src/screens/Settings/username.tsx
@@ -3,7 +3,8 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import styled from '@xstyled/styled-components';
-import React, { useContext, useEffect,useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { FieldError, useForm } from 'react-hook-form';
 
 import { NotificationContext } from '../../context/NotificationContext';
 import { UserDetailsContext } from '../../context/UserDetailsContext';
@@ -14,6 +15,7 @@ import Button from '../../ui-components/Button';
 import FilteredError from '../../ui-components/FilteredError';
 import { Form } from '../../ui-components/Form';
 import cleanError from '../../util/cleanError';
+import messages from '../../util/messages';
 
 interface Props {
 	className?: string
@@ -22,37 +24,24 @@ interface Props {
 const Username = ({ className }:Props): JSX.Element => {
 	const [editing, setEditing] = useState(false);
 	const [username, setUsername] = useState<string | null | undefined>('');
-	const [password, setPassword] = useState<string | null | undefined>('');
 	const currentUser = useContext(UserDetailsContext);
 	const [changeUsernameMutation, { loading, error }] = useChangeUsernameMutation();
 	const { queueNotification } = useContext(NotificationContext);
+	const { errors, handleSubmit, register } = useForm();
 
 	useEffect(() => {
 		setUsername(currentUser.username);
 	}, [currentUser.username]);
-
-	const onUserNameChange = (event: React.ChangeEvent<HTMLInputElement>) => setUsername(event.currentTarget.value);
-	const onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => setPassword(event.currentTarget.value);
 
 	const handleEdit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>):void => {
 		event.preventDefault();
 		setEditing(true);
 	};
 
-	const handleChange = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>):void => {
-		event.preventDefault();
-		event.stopPropagation();
+	const handleSubmitForm = (data: Record<string, any>):void => {
+		const { username, password } = data;
 
-		if (!password) {
-			queueNotification({
-				header: 'Failed!',
-				message: 'Please type your password',
-				status: NotificationStatus.ERROR
-			});
-			return;
-		}
-
-		if (username) {
+		if (username && password) {
 			changeUsernameMutation({
 				variables: {
 					password,
@@ -89,19 +78,25 @@ const Username = ({ className }:Props): JSX.Element => {
 	};
 
 	return (
-		<Form standalone={false} className={className}>
+		<Form standalone={false} className={className} onSubmit={handleSubmit(handleSubmitForm)}>
 			<Form.Group>
 				<Form.Field width={10}>
 					<label>Username</label>
 					{editing ?
 						<input
-							value={username || ''}
-							onChange={onUserNameChange}
+							className={errors.username ? 'error' : ''}
+							defaultValue={username || ''}
+							name='username'
 							placeholder='username'
+							ref={register({ maxLength:30, minLength:3, pattern: /^[A-Za-z0-9._-]*$/, required: true })}
 							type='text'
 						/> :
 						<div className='current-username'>{username}</div>
 					}
+					{(errors.username as FieldError)?.type === 'maxLength' && <span className={'errorText'}>{messages.VALIDATION_USERNAME_MAXLENGTH_ERROR}</span>}
+					{(errors.username as FieldError)?.type === 'minLength' && <span className={'errorText'}>{messages.VALIDATION_USERNAME_MINLENGTH_ERROR}</span>}
+					{(errors.username as FieldError)?.type === 'pattern' && <span className={'errorText'}>{messages.VALIDATION_USERNAME_PATTERN_ERROR}</span>}
+					{(errors.username as FieldError)?.type === 'required' && <span className={'errorText'}>{messages.VALIDATION_USERNAME_REQUIRED_ERROR}</span>}
 				</Form.Field>
 				{!editing && <Form.Field width={6}>
 					<label>&nbsp;</label>
@@ -118,19 +113,20 @@ const Username = ({ className }:Props): JSX.Element => {
 				<Form.Field width={10}>
 					<label>Password</label>
 					<input
-						value={password || ''}
-						onChange={onPasswordChange}
+						className={errors.password ? 'error' : ''}
+						name='password'
 						placeholder='password'
 						type='password'
+						ref={register({ minLength: 6 ,required: true })}
 					/>
 					{error && <FilteredError text={error.message}/>}
+					{errors.password && <span className={'errorText'}>{messages.VALIDATION_PASSWORD_ERROR}</span>}
 				</Form.Field>
 				<Form.Field width={6}>
 					<label>&nbsp;</label>
 					<Button
 						secondary
 						disabled={loading}
-						onClick={handleChange}
 						type='submit'
 					>
 					Change
@@ -150,5 +146,13 @@ export default styled(Username)`
 
 	.current-username {
 		padding-top: 1rem;
+	}
+
+	input.error {
+		border-color: red_secondary !important;
+	}
+
+	.errorText {
+		color: red_secondary;
 	}
 `;
