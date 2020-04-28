0777f5449d5ac760fb990447b18753991e69f8af
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2020-04-28 11:33:56 +0200

    Replace hash with number (#726)
    * replace hash with number
    
    * clean ups

--- chain-db-watcher/src/queries.ts
+++ chain-db-watcher/src/queries.ts
@@ -28,27 +28,6 @@ export const proposalSubscription = gql`
 	}
 `;
 
-// e.g returns
-// data: {
-//  proposal: {
-//      mutation: 'CREATED',
-//      node: {
-//         author: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
-//         method: 'remark',
-//         metaDescription: '[ Make some on-chain remark.]',
-//         proposalId: 0,
-//         section: 'system',
-//         depositAmount: '123000000000000',
-//         proposalArguments: [
-//             {
-//             name: '_remark',
-//             value: '0x00'
-//             }
-//         ]
-//      }
-//   }
-// };
-
 export const referendumSubscription = gql`
 	subscription referendumSubscription($startBlock: Int!) {
 		referendum (
@@ -69,7 +48,7 @@ export const referendumSubscription = gql`
 				referendumId
 				referendumStatus(orderBy: id_DESC) {
 					blockNumber {
-						hash
+						number
 					}
 					status
 				}
@@ -78,22 +57,6 @@ export const referendumSubscription = gql`
 		}
 	}
 `;
-// "referendum": {
-//   "mutation": "CREATED",
-//   "node": {
-//     "id": 16,
-//     "referendumId": 0,
-//     "referendumStatus": [
-//       {
-//         "blockNumber": {
-//           "number": 60
-//         },
-//         "status": "Started"
-//       }
-//     ],
-//     "preimageHash": "0xd81b9aefdaf562df8e7b503523eeca599ed89ef91e4828da82fb2030ca15d01e"
-//   }
-// }
 
 export const motionSubscription = gql`
 	subscription motionSubscription($startBlock: Int!) {
@@ -133,25 +96,6 @@ export const motionSubscription = gql`
     }
 `;
 
-// "motion": {
-//     "mutation": "CREATED",
-//     "node": {
-//         "motionProposalId": 0,
-//         "preimage": {
-//             "hash": "0x24f65d1cc0dcbf025a12c7fb969f7251b576155c9bff24b6e638c21ab3b3897b"
-//         },
-//         "id": 6,
-//         "motionStatus": [
-//             {
-//                 "blockNumber": {
-//                     "hash": "0xf54c4d8f46b9a9e770e1f595ea5ac545fdfe5b9cc485ff425dc72464bf2815f0"
-//                 },
-//                 "status": "Proposed"
-//             }
-//         ],
-//     }
-// }
-
 export const treasurySpendProposalSubscription = gql`
 	subscription treasurySpendProposalSubscription($startBlock: Int!) {
         treasurySpendProposal (
@@ -181,24 +125,3 @@ export const treasurySpendProposalSubscription = gql`
         }
     }
 `;
-
-// {
-// "data": {
-//   "treasurySpendProposal": {
-//     "mutation": "CREATED",
-//     "node": {
-//       "id": 3,
-//       "proposer": "5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw",
-//       "treasuryProposalId": 0,
-//       "treasuryStatus": [
-//       {
-//         "blockNumber": {
-//           "hash": "0x8a0367f7891bbc2a17d0566224ca929de3b0b0dbc4bb852316293f777d1d53db"
-//         },
-//         "status": "Proposed"
-//         }
-//       ]
-//     }
-//   }
-// }
-//   }
