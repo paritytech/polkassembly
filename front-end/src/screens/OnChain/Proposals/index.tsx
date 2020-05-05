aae5292533dcb8c2ca643d8f9cfae0080c3133e9
Nikhil Ranjan <niklabh811@gmail.com>
2020-05-05 15:19:05 +0530

    Added refetch in useEffect to reload page on react navigation (#744)
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>

--- front-end/src/screens/OnChain/Proposals/index.tsx
+++ front-end/src/screens/OnChain/Proposals/index.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import ProposalsListing from '../../../components/Listings/ProposalsListing';
 import { useLatestDemocracyProposalPostsQuery } from '../../../generated/graphql';
@@ -17,11 +17,15 @@ interface Props {
 
 const ProposalsContainer = ({ className }:Props) => {
 
-	const { data, error } = useLatestDemocracyProposalPostsQuery({ variables: {
+	const { data, error, refetch } = useLatestDemocracyProposalPostsQuery({ variables: {
 		limit: 5,
 		postTopic: post_topic.DEMOCRACY,
 		postType: post_type.ON_CHAIN
 	} });
+
+	useEffect(() => {
+		refetch();
+	}, [refetch]);
 
 	if (error?.message) return <FilteredError text={error.message}/>;
 
