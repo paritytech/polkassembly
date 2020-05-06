fd1c5ef312658c376f2fadc4fbab7811cf3fa37d
Nikhil Ranjan <niklabh811@gmail.com>
2020-05-06 15:53:45 +0530

    Not locading account

--- front-end/src/screens/Settings/setCredentials.tsx
+++ front-end/src/screens/Settings/setCredentials.tsx
@@ -2,18 +2,16 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { web3Accounts, web3Enable, web3FromSource } from '@polkadot/extension-dapp';
-import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
+import { web3Accounts, web3Enable, web3FromAddress } from '@polkadot/extension-dapp';
 import { stringToHex } from '@polkadot/util';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { FieldError,useForm } from 'react-hook-form';
-import { DropdownProps } from 'semantic-ui-react';
 
 import ExtensionNotDetected from '../../components/ExtensionNotDetected';
 import { UserDetailsContext } from '../../context/UserDetailsContext';
 import { useSetCredentialsConfirmMutation, useSetCredentialsStartMutation } from '../../generated/graphql';
 import { handleTokenChange } from '../../services/auth.service';
-import AccountSelectionForm from '../../ui-components/AccountSelectionForm';
+import AddressComponent from '../../ui-components/Address';
 import Button from '../../ui-components/Button';
 import FilteredError from '../../ui-components/FilteredError';
 import { Form } from '../../ui-components/Form';
@@ -29,7 +27,6 @@ const SetCredentials = (): JSX.Element => {
 	const [error, setErr] = useState<Error | null>(null);
 	const [showModal, setShowModal] = useState(false);
 	const [address, setAddress] = useState<string>('');
-	const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
 	const [isAccountLoading, setIsAccountLoading] = useState(true);
 	const [extensionNotFound, setExtensionNotFound] = useState(false);
 	const [accountsNotFound, setAccountsNotFound] = useState(false);
@@ -38,12 +35,7 @@ const SetCredentials = (): JSX.Element => {
 	const [setCredentialsConfirmMutation, { loading }] = useSetCredentialsConfirmMutation();
 	const currentUser = useContext(UserDetailsContext);
 
-	useEffect(() => {
-		if (!accounts.length) {
-			getAccounts();
-		}
-	}, [accounts.length]);
-
+	// Accounts needs to be get to get default account signer
 	const getAccounts = async (): Promise<void> => {
 		const extensions = await web3Enable(APPNAME);
 
@@ -67,31 +59,25 @@ const SetCredentials = (): JSX.Element => {
 
 		accounts.forEach((account) => {
 			account.address = getEncodedAddress(account.address) || account.address;
-		});
 
-		setAccounts(accounts);
-		if (accounts.length > 0) {
-			setAddress(accounts[0].address);
-		}
+			if (account.address === currentUser.defaultAddress) {
+				setAddress(currentUser.defaultAddress);
+			}
+		});
 
 		setIsAccountLoading(false);
 		return;
 	};
 
-	const onAccountChange = (event: React.SyntheticEvent<HTMLElement, Event>, data: DropdownProps) => {
-		const addressValue = data.value as string;
-		setAddress(addressValue);
-	};
-
 	const handleSetCredentials = async (data: Record<string, any>): Promise<void> => {
-		if (!accounts.length) {
-			return getAccounts();
-		}
-
 		const { username, password } = data;
 
+		if (!address) {
+			return console.error('Default address not set');
+		}
+
 		try {
-			const injected = await web3FromSource(accounts[0].meta.source);
+			const injected = await web3FromAddress(address);
 			const signRaw = injected && injected.signer && injected.signer.signRaw;
 
 			if (!signRaw) {
@@ -137,12 +123,19 @@ const SetCredentials = (): JSX.Element => {
 		setShowModal(false);
 	};
 
-	const openModal = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>):void => {
+	const openModal = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): Promise<void> => {
 		event.preventDefault();
 		event.stopPropagation();
 
+		await getAccounts();
+
 		setShowModal(true);
 	};
+
+	const accountNotFoundCard = <div className='card'>
+		<div className='text-muted'>Default address account not found in Polkadot-js extenstion.</div>
+		<div className='text-muted'>Please reload this page after adding default address.</div>
+	</div>;
 
 	return (
 		<>
@@ -177,59 +170,54 @@ const SetCredentials = (): JSX.Element => {
 							</div>
 							: null
 						}
-						{accountsNotFound?
-							<div className='card'>
-								<div className='text-muted'>You need at least one account in Polkadot-js extenstion to login.</div>
-								<div className='text-muted'>Please reload this page after adding accounts.</div>
-							</div>
-							: null
-						}
+						{ accountsNotFound? accountNotFoundCard : null }
 						{isAccountLoading
 							?
 							<Loader text={'Requesting Web3 accounts'}/>
 							:
-							accounts.length > 0 &&
-							<>
-								<Form.Group>
-									<AccountSelectionForm
-										title='Choose linked account'
-										accounts={accounts}
-										address={address}
-										onAccountChange={onAccountChange}
-									/>
-								</Form.Group>
-							</>
+							address ?
+								<>
+									<Form.Group>
+										<Form.Field width={16}>
+											<label>Default Address</label>
+											<AddressComponent address={address} />
+										</Form.Field>
+									</Form.Group>
+								</>
+								: accountNotFoundCard
 						}
-						<Form.Group>
-							<Form.Field width={16}>
-								<label>Username</label>
-								<input
-									className={errors.username ? 'error' : ''}
-									name='username'
-									placeholder='John'
-									ref={register(validation.username)}
-									type='text'
-								/>
-								{(errors.username as FieldError)?.type === 'maxLength' && <span className={'errorText'}>{messages.VALIDATION_USERNAME_MAXLENGTH_ERROR}</span>}
-								{(errors.username as FieldError)?.type === 'minLength' && <span className={'errorText'}>{messages.VALIDATION_USERNAME_MINLENGTH_ERROR}</span>}
-								{(errors.username as FieldError)?.type === 'pattern' && <span className={'errorText'}>{messages.VALIDATION_USERNAME_PATTERN_ERROR}</span>}
-								{(errors.username as FieldError)?.type === 'required' && <span className={'errorText'}>{messages.VALIDATION_USERNAME_REQUIRED_ERROR}</span>}
-							</Form.Field>
-						</Form.Group>
-						<Form.Group>
-							<Form.Field width={16}>
-								<label>Password</label>
-								<input
-									className={errors.password ? 'error' : ''}
-									name='password'
-									placeholder='Password'
-									ref={register(validation.password)}
-									type='password'
-								/>
-								{errors.password && <span className={'errorText'}>{messages.VALIDATION_PASSWORD_ERROR}</span>}
-							</Form.Field>
-						</Form.Group>
-
+						{address ? <>
+							<Form.Group>
+								<Form.Field width={16}>
+									<label>Username</label>
+									<input
+										className={errors.username ? 'error' : ''}
+										name='username'
+										placeholder='John'
+										ref={register(validation.username)}
+										type='text'
+									/>
+									{(errors.username as FieldError)?.type === 'maxLength' && <span className={'errorText'}>{messages.VALIDATION_USERNAME_MAXLENGTH_ERROR}</span>}
+									{(errors.username as FieldError)?.type === 'minLength' && <span className={'errorText'}>{messages.VALIDATION_USERNAME_MINLENGTH_ERROR}</span>}
+									{(errors.username as FieldError)?.type === 'pattern' && <span className={'errorText'}>{messages.VALIDATION_USERNAME_PATTERN_ERROR}</span>}
+									{(errors.username as FieldError)?.type === 'required' && <span className={'errorText'}>{messages.VALIDATION_USERNAME_REQUIRED_ERROR}</span>}
+								</Form.Field>
+							</Form.Group>
+							<Form.Group>
+								<Form.Field width={16}>
+									<label>Password</label>
+									<input
+										className={errors.password ? 'error' : ''}
+										name='password'
+										placeholder='Password'
+										ref={register(validation.password)}
+										type='password'
+									/>
+									{errors.password && <span className={'errorText'}>{messages.VALIDATION_PASSWORD_ERROR}</span>}
+								</Form.Field>
+							</Form.Group>
+						</> : null
+						}
 						{error?.message && <FilteredError text={error.message}/>}
 					</Form>
 				</Modal>
