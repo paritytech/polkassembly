ba0eadb08a349cbdb2de334ce1241b2d9fe87ab3
erler <mail@michelerler.com>
2020-05-12 10:27:41 +0200

    getEncoded Address with mocked getNetwork & lint

--- front-end/src/util/__tests__/formatBnBalance.test.ts
+++ front-end/src/util/__tests__/formatBnBalance.test.ts
@@ -3,7 +3,7 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 import formatBnBalance from '../formatBnBalance';
 
-jest.mock('../getNetwork', () => jest.fn(() => {return 'polkadot'}) );
+jest.mock('../getNetwork', () => jest.fn(() => {return 'polkadot';}));
 const getNetwork = require('../getNetwork');
 
 describe('Formatting BN Balances', () => {
@@ -17,22 +17,22 @@ describe('Formatting BN Balances', () => {
 	const formattedBalance4 = '53.0 DOT';
 
 	it('in Kusama', () => {
-		getNetwork.mockImplementation(() => 'kusama')
+		getNetwork.mockImplementation(() => 'kusama');
 		expect(formatBnBalance('1000000000000', { numberAfterComma: 2 } )).toEqual(formattedBalance1);
 		expect(formatBnBalance('1000000000000000000', { numberAfterComma: 2 } )).not.toEqual(formattedBalance1);
 		expect(formatBnBalance('1000000000000000', { withThousandDelimitor: true } )).not.toEqual(formattedBalance2);
 		expect(formatBnBalance('1000000000000000', { numberAfterComma: 0, withThousandDelimitor: true } )).toEqual(formattedBalance2);
 		expect(formatBnBalance('53000000000000', { numberAfterComma: 1, withUnit: true } )).toEqual(formattedBalance3);
 		expect(formatBnBalance('53000000000000000000', { numberAfterComma: 1, withUnit: true } )).not.toEqual(formattedBalance4);
-	})
+	});
 
 	it('in Polkadot', () => {
-		getNetwork.mockImplementation(() => 'polkadot')
+		getNetwork.mockImplementation(() => 'polkadot');
 		expect(formatBnBalance('1000000000000', { numberAfterComma: 2 } )).not.toEqual(formattedBalance1);
 		expect(formatBnBalance('1000000000000000000', { numberAfterComma: 2 } )).toEqual(formattedBalance1);
 		expect(formatBnBalance('1000000000000', { withThousandDelimitor: true } )).not.toEqual(formattedBalance2);
 		expect(formatBnBalance('1000000000000000000000', { numberAfterComma: 0, withThousandDelimitor: true } )).toEqual(formattedBalance2);
 		expect(formatBnBalance('53000000000000', { numberAfterComma: 1, withUnit: true } )).not.toEqual(formattedBalance3);
 		expect(formatBnBalance('53000000000000000000', { numberAfterComma: 1, withUnit: true } )).toEqual(formattedBalance4);
-	})
+	});
 });
---
+++ front-end/src/util/__tests__/getEncodedAddress.test.ts
@@ -0,0 +1,28 @@
+// Copyright 2019-2020 @paritytech/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import getEncodedAddress from '../getEncodedAddress';
+
+jest.mock('../getNetwork', () => jest.fn(() => {return 'polkadot';}));
+const getNetwork = require('../getNetwork');
+
+describe('Get Encoded Address', () => {
+	beforeEach(() => {
+		jest.resetModules();
+	});
+
+	const address1 = 'FqfW96FuXRH7pRy9njeNSxu4CNoBM1hKnVAmNRrFrU2izwj';
+
+	it('for Kusama', () => {
+		getNetwork.mockImplementation(() => 'kusama');
+		expect(getEncodedAddress(address1)).toBe('FqfW96FuXRH7pRy9njeNSxu4CNoBM1hKnVAmNRrFrU2izwj');
+		expect(getEncodedAddress(address1)).not.toBe('A41F9YrXEYYxm2WFFrczYhUQiEW8APB8qw39gnYnzsYSZ9M');
+	});
+
+	it('for Polkadot', () => {
+		getNetwork.mockImplementation(() => 'polkadot');
+		expect(getEncodedAddress(address1)).not.toBe('FqfW96FuXRH7pRy9njeNSxu4CNoBM1hKnVAmNRrFrU2izwj');
+		expect(getEncodedAddress(address1)).toBe('A41F9YrXEYYxm2WFFrczYhUQiEW8APB8qw39gnYnzsYSZ9M');
+	});
+});
