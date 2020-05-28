f66db8d60206bf02e2e4a3b539d325a343eaa943
thib <thibaut.sardan@gmail.com>
2020-05-28 22:39:21 +0200

    with help

--- launch.sh
+++ launch.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+while getopts "frh" arg; do
+	case $arg in
+		    h | *) # Display help.
+	      echo "Usage: $0 -f for node-watcher -fr for node-watcher with reset" 1>&2; exit 1;
+	      exit 0
+	      ;;
+	esac
+done
+
 printf "\n-- stoping docker hasura_graphql-engine_1 hasura_postgres_1 auth-server_postgres_1\n"
 docker stop hasura_graphql-engine_1 hasura_postgres_1 auth-server_postgres_1;
 
@@ -24,33 +34,29 @@ cd auth-server && yarn migrate &> auth-migration.log &
 sleep 5
 
 while getopts "frh" arg; do
-  case $arg in
-    f) 	printf "\n\n-- stoping node watcher docker\n"
-		docker stop node-watcher_postgres_1 node-watcher_prisma_1
-		printf "\n\n-- launching chain-db\n"
-		cd node-watcher && docker-compose up -d;
+	case $arg in
+	    f) 	printf "\n\n-- stoping node watcher docker\n"
+			docker stop node-watcher_postgres_1 node-watcher_prisma_1
+			printf "\n\n-- launching chain-db\n"
+			cd node-watcher && docker-compose up -d;
 
-		while getopts "r" arg2; do
-  			case $arg2 in
-			    r) printf "\n-- reset node watcher\n"
-					sleep 5
-					yarn reset -f;
-			      ;;
-      		esac
-      	done
+			while getopts "r" arg2; do
+	  			case $arg2 in
+				    r) 	printf "\n-- reset node watcher\n"
+						sleep 5
+						yarn reset -f;
+				      	;;
+	      		esac
+	      	done
 
-		printf "\n\n-- launching node watcher\n"
-	  	yarn local &> node-watcher.log &
-		
-		printf "\n-- launch chain-db-open-server\n"
-		cd ../chain-db-open-server && yarn start:local &> chain-db-open-server.log &
-		cd ../
-		;;
-    h | *) # Display help.
-      echo "Usage: $0 -f for node-watcher -fr for node-watcher with reset" 1>&2; exit 1;
-      exit 0
-      ;;
-  esac
+			printf "\n\n-- launching node watcher\n"
+		  	yarn local &> node-watcher.log &
+			
+			printf "\n-- launch chain-db-open-server\n"
+			cd ../chain-db-open-server && yarn start:local &> chain-db-open-server.log &
+			cd ../
+			;;
+	esac
 done
 
 printf "\n\n-- launching hasura docker\n"
