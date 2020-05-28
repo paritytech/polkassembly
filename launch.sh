739c8c4296a01784c66e34a45a45881522a5f045
thib <thibaut.sardan@gmail.com>
2020-05-28 23:51:06 +0200

    use dir and remove help I cannot get that to work

--- launch.sh
+++ launch.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-while getopts "h" arg; do
-	case $arg in
-		    h | *) # Display help.
-				echo "Usage: $0 -f for node-watcher -fr for node-watcher with reset" 1>&2; exit 1;
-				exit 0
-				;;
-	esac
-done
+# getopts "hfr" args;
+# case $args in
+# 	    h) # Display help.
+# 			echo "Usage: $0 -f for node-watcher -fr for node-watcher with reset" 1>&2; exit 1;
+# 			exit 0
+# 			;;
+# 		*)	;;
+# esac
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 printf "\n-- stoping docker hasura_graphql-engine_1 hasura_postgres_1 auth-server_postgres_1\n"
 docker stop hasura_graphql-engine_1 hasura_postgres_1 auth-server_postgres_1;
@@ -19,29 +21,33 @@ printf "\n\n-- killing any node running\n"
 pkill -f node;
 
 printf "\n\n-- launching auth-server docker\n"
-cd auth-server && docker-compose --file docker-compose.yaml up -d && cd ..;
+cd $DIR/auth-server && docker-compose --file docker-compose.yaml up -d;
 
 sleep 5
 
 printf "\n\n-- launching auth-server\n" 
-cd auth-server && yarn && yarn start &> auth.log &
+cd $DIR/auth-server && yarn && yarn start &> auth.log &
 
 sleep 5
 
 printf "\n\n-- migrating auth-server\n" 
-cd auth-server && yarn migrate &> auth-migration.log &
+cd $DIR/auth-server && yarn migrate &> auth-migration.log &
 
 sleep 5
 
-while getopts "fr" arg; do
-	case $arg in
+while getopts "fr" args; do
+	case $args in
 		f) 	printf "\n\n-- stoping node watcher docker\n"
 			docker stop node-watcher_postgres_1 node-watcher_prisma_1
-			printf "\n\n-- launching chain-db\n"
-			cd node-watcher && docker-compose up -d;
+			
+			printf "\n\n-- launching chain-db-watcher"
+			cd $DIR/chain-db-watcher && yarn start &> chain-db-watcher.log &
 
-			while getopts "r" arg2; do
-					case $arg2 in
+			printf "\n\n-- launching chain-db docker\n"
+			cd $DIR/node-watcher && docker-compose up -d;
+
+			while getopts "r" args2; do
+					case $args2 in
 					r) 	printf "\n-- reset node watcher\n"
 						sleep 5
 						yarn reset -f;
@@ -50,25 +56,24 @@ while getopts "fr" arg; do
 				done
 
 			printf "\n\n-- launching node watcher\n"
-				yarn local &> node-watcher.log &
+			yarn local &> node-watcher.log &
 
 			printf "\n-- launch chain-db-open-server\n"
-			cd ../chain-db-open-server && yarn start:local &> chain-db-open-server.log &
-			cd ../
+			cd $DIR/chain-db-open-server && yarn start:local &> chain-db-open-server.log &
 			;;
 	esac
 done
 
 printf "\n\n-- launching hasura docker\n"
-cd hasura && docker-compose up -d && cd ..;
+cd $DIR/hasura && docker-compose up -d;
 
-sleep 5
+sleep 15
 
 printf "\n\n-- hasura migration\n"
-cd hasura/hasura-migrations/migrations && hasura-dev migrate apply --admin-secret ip7itpC5L6R3RbNE4vkdZfsN && cd ../../..;
+cd $DIR/hasura/hasura-migrations/migrations && hasura-dev migrate apply --admin-secret ip7itpC5L6R3RbNE4vkdZfsN;
 
 # printf "\n\n-- hasura reload metadata\n"
-# cd hasura/hasura-migrations && hasura-dev metadata reload --admin-secret ip7itpC5L6R3RbNE4vkdZfsN && cd ../..;
+# cd $DIR/hasura/hasura-migrations && hasura-dev metadata reload --admin-secret ip7itpC5L6R3RbNE4vkdZfsN;
 
 printf "\n\n-- launching front-end\n"
-cd front-end && yarn && yarn start &> front-end.log &
+cd $DIR/front-end && yarn &&  yarn start
