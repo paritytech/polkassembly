86c7d4f33af57645640d7533a1212a73db8207be
thib <thibaut.sardan@gmail.com>
2020-05-28 22:42:21 +0200

    with tab

--- launch.sh
+++ launch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-while getopts "frh" arg; do
+while getopts "h" arg; do
 	case $arg in
 		    h | *) # Display help.
-	      echo "Usage: $0 -f for node-watcher -fr for node-watcher with reset" 1>&2; exit 1;
-	      exit 0
-	      ;;
+				echo "Usage: $0 -f for node-watcher -fr for node-watcher with reset" 1>&2; exit 1;
+				exit 0
+				;;
 	esac
 done
 
@@ -33,25 +33,25 @@ cd auth-server && yarn migrate &> auth-migration.log &
 
 sleep 5
 
-while getopts "frh" arg; do
+while getopts "fr" arg; do
 	case $arg in
-	    f) 	printf "\n\n-- stoping node watcher docker\n"
+		f) 	printf "\n\n-- stoping node watcher docker\n"
 			docker stop node-watcher_postgres_1 node-watcher_prisma_1
 			printf "\n\n-- launching chain-db\n"
 			cd node-watcher && docker-compose up -d;
 
 			while getopts "r" arg2; do
-	  			case $arg2 in
-				    r) 	printf "\n-- reset node watcher\n"
+					case $arg2 in
+					r) 	printf "\n-- reset node watcher\n"
 						sleep 5
 						yarn reset -f;
-				      	;;
-	      		esac
-	      	done
+						;;
+					esac
+				done
 
 			printf "\n\n-- launching node watcher\n"
-		  	yarn local &> node-watcher.log &
-			
+				yarn local &> node-watcher.log &
+
 			printf "\n-- launch chain-db-open-server\n"
 			cd ../chain-db-open-server && yarn start:local &> chain-db-open-server.log &
 			cd ../
