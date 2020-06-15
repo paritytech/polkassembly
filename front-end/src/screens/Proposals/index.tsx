7c50dfe2f79be9536d5ae42120a4c47055f98559
thib <thibaut.sardan@gmail.com>
2020-06-15 11:11:52 +0200

    remove latest and inconsistant casing

--- front-end/src/screens/Proposals/index.tsx
+++ front-end/src/screens/Proposals/index.tsx
@@ -13,7 +13,7 @@ const OnchainPostsContainer = ({ className } : {className?: string}) => {
 
 	return (
 		<div className={className}>
-			<h1>Latest On Chain Proposals</h1>
+			<h1>On-chain proposals</h1>
 			<Grid stackable reversed='mobile tablet'>
 				<Grid.Column mobile={16} tablet={16} computer={10}>
 					<ProposalContainer limit={15} />
