9af4001cadeb3e2d1a6638e07e85482e8167fcef
Nikhil Ranjan <niklabh811@gmail.com>
2020-06-19 21:24:52 +0530

    OG meta tags in head for social media sharing (#781)
    * helmet
    
    * Using context
    
    * Using context
    
    * setting description and title
    
    * removed unused div
    
    * fix
    
    * lint
    
    * Add proposal id info into the title
    
    Co-authored-by: thib <thibaut.sardan@gmail.com>
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>

---
+++ front-end/src/context/MetaContext.tsx
@@ -0,0 +1,37 @@
+// Copyright 2019-2020 @paritytech/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import React, { createContext, useState } from 'react';
+
+import { MetaContextType } from '../types';
+import getNetwork from '../util/getNetwork';
+
+const network = getNetwork();
+
+const title = `Polkassembly | ${network}`;
+const description = `polkassembly, discussion platform for ${network} governance`;
+
+const initialMetaContext : MetaContextType = {
+	description,
+	image: `https://${network}.polkassembly.io/static/media/polkassembly-logo.aa0e69ae.png`,
+	setMetaContextState : (): void => {
+		throw new Error('setMetaContextState function must be overridden');
+	},
+	title,
+	type: 'website',
+	url: `https://${network}.polkassembly.io/`
+};
+
+export const MetaContext = createContext(initialMetaContext);
+
+export const MetaProvider = ({ children }: React.PropsWithChildren<{}>) => {
+
+	const [metaContextState, setMetaContextState] = useState(initialMetaContext);
+
+	return (
+		<MetaContext.Provider value={{ ...metaContextState, setMetaContextState }}>
+			{children}
+		</MetaContext.Provider>
+	);
+};
