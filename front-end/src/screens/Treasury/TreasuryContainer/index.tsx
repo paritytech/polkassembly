9433f975b20dfa8739eba5a30ef4235d1df30150
Nikhil Ranjan <niklabh811@gmail.com>
2020-08-07 13:49:57 +0530

    Added pagination (#985)
    * Added pagination
    
    * Added load more buttons for listings
    
    * no offset
    
    * 20

--- front-end/src/screens/Treasury/TreasuryContainer/index.tsx
+++ front-end/src/screens/Treasury/TreasuryContainer/index.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import TreasuryListing from '../../../components/Listings/TreasuryListing';
 import { useAllDemocracyTreasuryProposalPostsQuery } from '../../../generated/graphql';
@@ -10,6 +10,7 @@ import { post_topic } from '../../../global/post_topics';
 import { post_type } from '../../../global/post_types';
 import FilteredError from '../../../ui-components/FilteredError';
 import Loader from '../../../ui-components/Loader';
+import LoadMore from '../../../ui-components/LoadMore';
 
 interface Props {
 	className?: string
@@ -17,8 +18,9 @@ interface Props {
 }
 
 const TreasuryProposalsContainer = ({ className, limit }:Props) => {
+	const [page, setPage] = useState(1);
 
-	const { data, error, refetch } = useAllDemocracyTreasuryProposalPostsQuery({ variables: {
+	const { data, error, loading, refetch } = useAllDemocracyTreasuryProposalPostsQuery({ variables: {
 		limit,
 		postTopic: post_topic.TREASURY,
 		postType: post_type.ON_CHAIN
@@ -28,9 +30,18 @@ const TreasuryProposalsContainer = ({ className, limit }:Props) => {
 		refetch();
 	}, [refetch]);
 
+	const loadMore = () => {
+		setPage(page + 1);
+	};
+
 	if (error?.message) return <FilteredError text={error.message}/>;
 
-	if (data) return <TreasuryListing className={className} data={data}/>;
+	if (data) return (
+		<>
+			<TreasuryListing className={className} data={data}/>
+			<LoadMore onClick={loadMore} loading={loading} />
+		</>
+	);
 
 	return <Loader/>;
 };
