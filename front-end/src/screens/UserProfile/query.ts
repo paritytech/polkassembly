73f692fd553f11ba6b0b4c5f3baf6b322a5fdf92
Nikhil Ranjan <niklabh811@gmail.com>
2020-08-29 20:43:43 +0530

    Added profile page with username (#1011)
    * Api bump 1.30.1
    
    * added profile query with username
    
    * Added profile query

---
+++ front-end/src/screens/UserProfile/query.ts
@@ -0,0 +1,16 @@
+// Copyright 2019-2020 @paritytech/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import gql from 'graphql-tag';
+
+export const PROFILE = gql`
+    query PROFILE($username: String!) {
+        profile(username: $username) {
+            id
+            username
+            kusama_default_address
+            polkadot_default_address
+        }
+    }
+`;
