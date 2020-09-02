db1ea22c05d31ebae3f0439882bc95f08e0c15dc
Nikhil Ranjan <niklabh811@gmail.com>
2020-09-02 14:40:56 +0530

    Discussion Sort by feature (#1018)
    * Added order by dropdown
    
    * implementation
    
    * types
    
    * loading

---
+++ front-end/src/global/sortOptions.ts
@@ -0,0 +1,15 @@
+// Copyright 2019-2020 @paritytech/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+export const sortValues = {
+	COMMENTED: 'commented',
+	NEWEST: 'newest',
+	OLDEST: 'oldest'
+};
+
+export const sortOptions = [
+	{ key: 'commented', text: 'Last Commented', value: sortValues.COMMENTED },
+	{ key: 'newest', text: 'Date Added (newest)', value: sortValues.NEWEST },
+	{ key: 'oldest', text: 'Date Added (oldest)', value: sortValues.OLDEST }
+];
--- front-end/src/screens/Discussions/query.ts
+++ front-end/src/screens/Discussions/query.ts
@@ -5,29 +5,54 @@
 import gql from 'graphql-tag';
 import { authorFields } from 'src/fragments/author';
 
-export const QUERY_LATEST_DISCUSSIONS = gql`
-    query LatestDiscussionPosts($limit: Int! = 20) {
-        posts(order_by: {last_update: {last_update: desc}}, limit: $limit, where: {type: {id: {_eq: 1}}}) {
+const postFields = gql`
+    fragment postFields on posts {
+        id
+        title
+        author {
+            ...authorFields
+        }
+        created_at
+        updated_at
+        comments_aggregate {
+            aggregate {
+                count
+            }
+        }
+        type {
+            name
             id
-            title
-            author {
-                ...authorFields
-            }
-            created_at
-            updated_at
-            comments_aggregate {
-                aggregate {
-                    count
-                }
-            }
-            type {
-                name
-                id
-            }
-            last_update {
-                last_update
-            }
+        }
+        last_update {
+            last_update
         }
     }
     ${authorFields}
+`;
+
+export const QUERY_LATEST_DISCUSSIONS = gql`
+    query LatestDiscussionPosts($limit: Int! = 20) {
+        posts(order_by: {last_update: {last_update: desc}}, limit: $limit, where: {type: {id: {_eq: 1}}}) {
+            ...postFields
+        }
+    }
+    ${postFields}
+`;
+
+export const QUERY_DISCUSSIONS_ID_DESC = gql`
+    query DiscussionPostsIdDesc($limit: Int! = 20) {
+        posts(order_by: {id: desc}, limit: $limit, where: {type: {id: {_eq: 1}}}) {
+            ...postFields
+        }
+    }
+    ${postFields}
+`;
+
+export const QUERY_DISCUSSIONS_ID_ASC = gql`
+    query DiscussionPostsIdAsc($limit: Int! = 20) {
+        posts(order_by: {id: asc}, limit: $limit, where: {type: {id: {_eq: 1}}}) {
+            ...postFields
+        }
+    }
+    ${postFields}
 `;
