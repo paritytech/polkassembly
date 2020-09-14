a1d5113dca1b8110bd12a7a49ae205723937be26
thib <thibaut.sardan@gmail.com>
2020-09-14 12:02:33 +0200

    remove stray logs

--- front-end/src/util/getEncodedAddress.ts
+++ front-end/src/util/getEncodedAddress.ts
@@ -1,11 +1,8 @@
 // Copyright 2019-2020 @paritytech/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-import { u8aToHex } from '@polkadot/util';
-// Copyright 2019-2020 @paritytech/polkassembly authors & contributors
-// This software may be modified and distributed under the terms
-// of the Apache-2.0 license. See the LICENSE file for details.
-import { decodeAddress,encodeAddress } from '@polkadot/util-crypto';
+
+import { encodeAddress } from '@polkadot/util-crypto';
 import { chainProperties } from 'src/global/networkConstants';
 import getNetwork from 'src/util/getNetwork';
 
@@ -16,7 +13,7 @@ import getNetwork from 'src/util/getNetwork';
  *
  */
 
-export default function (address: string): string|null {
+export default function (address: string): string | null {
 	const network = getNetwork();
 	const ss58Format = chainProperties?.[network]?.ss58Format;
 
@@ -25,13 +22,7 @@ export default function (address: string): string|null {
 	}
 
 	try{
-		const addressEncoded = encodeAddress(address, ss58Format);
-		console.log('decodeAddress', u8aToHex(decodeAddress(address)));
-		console.log('encodeAddress 2', encodeAddress('HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F', 2));
-		console.log('encodeAddress 42', encodeAddress('HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F', 42));
-		console.log('encodeAddress 1', encodeAddress('HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F', 1));
-
-		return addressEncoded;
+		return encodeAddress(address, ss58Format);
 	} catch(e) {
 		console.error('getEncodedAddress error', e);
 		return null;
