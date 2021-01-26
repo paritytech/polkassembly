6ee0a90f6a461f5aaecb251799e57a4b6f8d17de
Nikhil Ranjan <niklabh811@gmail.com>
2021-01-27 02:42:04 +0530

    Misc Fixes (#1092)
    * Redirecting to homepage on login if already loggedin
    
    * fix
    
    * Fix lint bug and removed storybook

--- front-end/src/screens/LoginForm/index.tsx
+++ front-end/src/screens/LoginForm/index.tsx
@@ -2,17 +2,27 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React, { useState } from 'react';
+import React, { useContext, useEffect,useState } from 'react';
 import { Grid } from 'semantic-ui-react';
 
 import Web2Login from '../../components/Login/Web2Login';
 import Web3Login from '../../components/Login/Web3Login';
+import { UserDetailsContext } from '../../context/UserDetailsContext';
+import { useRouter } from '../../hooks';
 
 interface Props {
 	className?: string
 }
 
 const Login = ({ className }: Props) => {
+	const currentUser = useContext(UserDetailsContext);
+	const { history } = useRouter();
+
+	useEffect(() => {
+		if (currentUser?.id) {
+			history.push('/');
+		}
+	}, [history, currentUser, currentUser?.id]);
 
 	const [displayWeb2, setDisplayWeb2] = useState(true);
 	const toggleWeb2Login = () => setDisplayWeb2(!displayWeb2);
