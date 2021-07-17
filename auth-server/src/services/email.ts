8e6465e4190594133b8ef002bab34211fc132b3b
Nikhil Ranjan <niklabh811@gmail.com>
2021-07-17 05:55:45 +0200

    Moderation

--- auth-server/src/services/email.ts
+++ auth-server/src/services/email.ts
@@ -23,7 +23,7 @@ import shortenHash from '../utils/shortenHash';
 
 const apiKey = process.env.SENDGRID_API_KEY;
 const FROM = 'noreply@polkassembly.io';
-const REPORT = 'polkassembly@parity.io';
+const REPORT = 'nikhil@parity.io';
 const DOMAIN = process.env.DOMAIN_NAME && process.env.DOMAIN_PROTOCOL ? `${process.env.DOMAIN_PROTOCOL}${process.env.DOMAIN_NAME}` : 'https://test.polkassembly.io';
 
 if (apiKey) {
